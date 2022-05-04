77fc4ae37ef847d9613d2e009b359a563c1b6425
GG-Bruse <BJY811@outlook.com>
2022-05-04 10:19:59 +0800

    2022.05.04exercise

---
+++ 2022.05.04练习.cpp
@@ -0,0 +1,12 @@
+/*
+字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+*/
+
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool isFlipedString(string s1, string s2) {
+        return s1.size() == s2.size() && (s1 + s1).find(s2) != string::npos;
+    }
+};
