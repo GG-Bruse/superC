c65a470735f9d0da015c444ee68268a2a3b302f0
GG-Bruse <BJY811@outlook.com>
2022-07-17 23:27:23 +0800

    Stack

--- 线性表之栈.cpp
+++ 线性表之栈.cpp
@@ -20,16 +20,97 @@
 
 
 
-
+//C语言利用动态数组实现栈(1) 
+/*
 #include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<assert.h>
 typedef int SKDataType; 
-struct stack
+typedef struct stack
 {
 	SKDataType* data;
-	
-};
+	int top;
+	int capacity;
+}Stack;
+void StackInit(Stack* ps)
+{
+	assert(ps);
+	ps->data = (SKDataType*)malloc(sizeof(SKDataType)*4);
+	if(ps->data == NULL)
+	{
+		printf("malloc fail\n");
+		exit(-1);
+	}
+	 
+	ps->capacity = 4;
+	ps->top = 0;//top指向栈顶元素的上一个位置 
+}
+void StackDestory(Stack* ps)
+{
+	assert(ps);
+	free(ps->data);
+	ps->data = NULL;
+	ps->capacity = ps->top = 0;
+}
+void StackPush(Stack* ps,SKDataType d)
+{
+	assert(ps);
+	if(ps->top == ps->capacity)
+	{
+		SKDataType* newSpace = (SKDataType*)realloc(ps->data,ps->capacity * 2 * sizeof(SKDataType));
+		if(newSpace == NULL)
+		{
+			printf("realloc fail\n");
+			exit(-1);
+		}
+		ps->data = newSpace;
+		ps->capacity *= 2;
+	}
+	ps->data[ps->top] = d;
+	++ps->top; 
+}
+void StackPop(Stack* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);//栈不可为空 
+	--ps->top;
+}
+SKDataType StackTop(Stack* ps)
+{
+	assert(ps);
+	assert(ps->top > 0);
+	return ps->data[ps->top - 1];
+}
+int StackSize(Stack* ps)
+{
+	assert(ps);
+	return ps->top;
+}
+bool StackIsEmpty(Stack* ps) 
+{
+	assert(ps);
+	return ps->top == 0;
+} 
 int main()
 {
+	Stack sk;
+	StackInit(&sk);
+	StackPush(&sk,1);
+	StackPush(&sk,2);
+	StackPush(&sk,3);
+	StackPush(&sk,4);
+	StackPush(&sk,5);
 	
+	while(!StackIsEmpty(&sk))
+	{
+		printf("%d ",StackTop(&sk));
+		StackPop(&sk);
+	}
+	
+	StackDestory(&sk);
 	return 0; 
 } 
+*/
+
+
