cd594181f6ad398a8f6915f1e1386d220666987f
GG-Bruse <BJY811@outlook.com>
2022-08-20 11:32:35 +0800

    C_exercise

--- 实验文件/源.c
+++ 实验文件/源.c
@@ -5304,3 +5304,175 @@ ABCD左旋两个字符得到CDAB
 
 
 
+
+//#include<stdio.h>
+//int main()
+//{
+//	char* p = "abcdef";
+//	printf("%d\n", sizeof(p));//4 char*
+//	printf("%d\n", sizeof(p + 1));//4 char*
+//	printf("%d\n", sizeof(*p));//1 char
+//	printf("%d\n", sizeof(p[0]));//1 char
+//	printf("%d\n", sizeof(&p));//4 char**
+//	printf("%d\n", sizeof(&p + 1));//4 char**
+//	printf("%d\n", sizeof(&p[0] + 1));//4 char*
+//
+//	printf("%d\n", strlen(p));//6
+//	printf("%d\n", strlen(p + 1));//5
+//	printf("%d\n", strlen(&p));//随机值
+//	printf("%d\n", strlen(&p + 1));//随机值
+//	printf("%d\n", strlen(&p[0] + 1));//5
+//	printf("%d\n", strlen(*p));//err
+//	printf("%d\n", strlen(p[0]));//err
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a));//48	3*4*4 = 48
+//	printf("%d\n", sizeof(a[0][0]));///4 int
+//	printf("%d\n", sizeof(a[0]));//16 int [4]
+//	printf("%d\n", sizeof(a[0] + 1));//第一行第二个元素的地址 4 int*
+//	printf("%d\n", sizeof(*(a[0] + 1)));//4	int
+//	printf("%d\n", sizeof(a + 1));//4 数组指针
+//	printf("%d\n", sizeof(*(a + 1)));//16 int [4]
+//	printf("%d\n", sizeof(&a[0] + 1));//4 数组指针
+//	printf("%d\n", sizeof(*(&a[0] + 1)));//16
+//	printf("%d\n", sizeof(*a));//16
+//	printf("%d\n", sizeof(a[3]));//16	sizeof在预处理阶段完成，表达式不实际运行
+//	return 0;
+//}
+
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d %d", *(a + 1), *(ptr - 1));//2 5
+//	return 0;
+//}
+
+
+
+
+
+//#include<stdio.h>
+////已知，结构体Test类型的变量大小是20个字节
+////假设p 的值为0x100000
+//struct Test
+//{
+//	int Num;
+//	char* pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}*p;
+//int main()
+//{
+//	p = (struct Test*)0x100000;
+//	printf("%p\n", p + 0x1);//00100014
+//	printf("%p\n", (unsigned long)p + 0x1);//00100001
+//	printf("%p\n", (unsigned int*)p + 0x1);//00100004
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[4] = { 1, 2, 3, 4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x %x", ptr1[-1], *ptr2);//4 2000000
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+//	int* p = a[0];
+//	printf("%d", p[0]);//1
+//	return 0;
+//}
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);//FFFFFFFC(-4以地址形式打印),-4
+//	return 0;
+//}
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));//10 5
+//	return 0;
+//}
+
+
+
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);//at
+//	return 0;
+//}
+
+
+
+
+
+//画图理解
+//#include<stdio.h>
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//	printf("%s\n", **++cpp);//POINT
+//	printf("%s\n", *-- * ++cpp + 3);//ER
+//	printf("%s\n", *cpp[-2] + 3);//ST
+//	printf("%s\n", cpp[-1][-1] + 1);//EW
+//	return 0;
+//}
