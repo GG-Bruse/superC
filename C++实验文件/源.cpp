8b55dd2fa2b055426c403837745afb7b60c1b94a
GG-Bruse <BJY811@outlook.com>
2022-09-10 23:34:19 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -8866,6 +8866,41 @@ s只包含小写字母
 
 
 
+/*
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+*/
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//class Solution
+//{
+//    const char* numToStr[10] = { "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };//缺省值声明
+//public:
+//    void Combine(string digits, size_t di, vector<string>& retV, string combineStr)
+//    {
+//        if (di == digits.size())
+//        {
+//            retV.push_back(combineStr);
+//            return;
+//        }
+//        size_t num = digits[di] - '0';
+//        string str = numToStr[num];
+//
+//        for (auto ch : str)
+//        {
+//            Combine(digits, di + 1, retV, combineStr + ch);
+//        }
+//    }
+//    vector<string> letterCombinations(string digits)
+//    {
+//        vector<string> retV;
+//        if (digits.empty())return retV;
+//        string str;
+//        Combine(digits, 0, retV, str);
+//        return retV;
+//    }
+//};
 
 
 
@@ -8873,16 +8908,142 @@ s只包含小写字母
 
 
 
-
-
-
-
-
-
-
-
-
-
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<cassert>
+//namespace bjy
+//{
+//	template<class T>
+//	class vector
+//	{
+//	public:
+//		typedef T value_type;
+//		typedef value_type* iterator;
+//		typedef const value_type* const_iterator;
+//		typedef value_type& reference;
+//		typedef const value_type& const_reference;
+//	public:
+//		iterator begin() { return _start; }
+//		iterator end() { return _finish; }
+//		const_iterator begin()const { return _start; }
+//		const_iterator end()const { return _finish; }
+//
+//		vector() :_start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {}
+//		~vector() {
+//			delete[] _start;
+//			_start = _finish = _end_of_storage = nullptr;
+//		}
+//
+//		size_t capacity()const { return _end_of_storage - _start; }
+//		size_t size()const { return _finish - _start; }
+//
+//		reference operator[](size_t pos) {
+//			assert(pos < size());
+//			return _start[pos];
+//		}
+//		const_reference& operator[](size_t pos)const {
+//			assert(pos < size());
+//			return _start[pos];
+//		}
+//
+//		void reserve(size_t n) {
+//			size_t previous_size = size();
+//			if (n > capacity()) {
+//				value_type* temp = new value_type[n];
+//				if (_start) {
+//					memcpy(temp, _start, sizeof(value_type) * size());
+//					delete[] _start;
+//				}
+//				_start = temp;
+//				_finish = _start + previous_size;
+//				_end_of_storage = _start + n;
+//			}
+//		}
+//		void insert(iterator pos, const_reference data) {
+//			assert((pos >= _start) && (pos <= _finish));
+//			if (_finish >= _end_of_storage) {
+//				size_t lenth = pos - _start;//记录,避免迭代器失效
+//				reserve(capacity() == 0 ? 6 : capacity() * 2);
+//				pos = _start + lenth;//更新
+//			}
+//			for (iterator cur = _finish - 1; cur >= pos; --cur)
+//				*(cur + 1) = *cur;
+//			*pos = data;
+//			++_finish;
+//		}
+//		iterator erase(iterator pos) {
+//			assert((pos >= _start) && (pos < _finish));
+//			for (iterator cur = pos + 1; cur < _finish; ++cur)
+//				*(cur - 1) = *cur;
+//			--_finish;
+//			return pos;
+//		}
+//		void push_back(const_reference data) {
+//			if (_finish >= _end_of_storage)
+//				reserve(capacity() == 0 ? 6 : capacity() * 2);
+//			*(_finish++) = data;
+//		}
+//		void pop_back() {
+//			assert(_finish > _start);
+//			--_finish;
+//		}
+//	private:
+//		iterator _start;
+//		iterator _finish;
+//		iterator _end_of_storage;
+//	};
+//}
+//
+//#include<algorithm>
+//using namespace bjy;
+//using std::cout;
+//using std::endl;
+//using std::find;
+//int main()
+//{
+//	vector<int>v;
+//	for (size_t i = 0; i < 10; ++i)
+//	{
+//		v.push_back(i + 1);
+//	}
+//
+//	vector<int>::iterator it = find(v.begin(), v.end(), 5);
+//	v.insert(it, 30);
+//	for (auto& e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	for (size_t i = 0; i < 10; ++i)
+//	{
+//		v.push_back(i + 1);
+//	}
+//
+//	it = find(v.begin(), v.end(), 5);
+//	v.erase(it);
+//	for (auto& e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	for (vector<int>::iterator it = v.begin(); it != v.end();) {
+//		if (*it % 2 == 0)
+//		{
+//			it = v.erase(it);
+//		}
+//		else {
+//			++it;
+//		}
+//	}
+//	for (auto e : v)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//	return 0;
+//}
 
 
 
