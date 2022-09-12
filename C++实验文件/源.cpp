ea03536fa98e5be873a617cf49b347e26f3e15aa
GG-Bruse <BJY811@outlook.com>
2022-09-13 00:34:27 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -9118,3 +9118,116 @@ s只包含小写字母
 //    cout << ret << endl;
 //    return 0;
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样
+*/
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Solution
+//{
+//public:
+//    void _reverseStr(string& s, size_t begin, size_t end)
+//    {
+//        while (begin < end)
+//        {
+//            char temp = s[begin];
+//            s[begin] = s[end];
+//            s[end] = temp;
+//            ++begin;
+//            --end;
+//        }
+//    }
+//    string reverseStr(string s, int k)
+//    {
+//        size_t begin = 0;
+//        size_t end = 0;
+//        size_t count = 0;
+//        while (count < s.size())
+//        {
+//            if ((count + 2 * k) <= s.size())
+//            {
+//                count += 2 * k;
+//                begin = count - 2 * k;
+//                end = count - k - 1;
+//                _reverseStr(s, begin, end);
+//            }
+//            else if ((count + k) <= s.size() && (count + 2 * k) > s.size())
+//            {
+//                count += 2 * k;
+//                begin = count - 2 * k;
+//                end = count - k - 1;
+//                _reverseStr(s, begin, end);
+//                break;
+//            }
+//            else if ((count + k) > s.size())
+//            {
+//                count += k;
+//                begin = count - k;
+//                end = s.size() - 1;
+//                _reverseStr(s, begin, end);
+//                break;
+//            }
+//        }
+//        return s;
+//    }
+//};
+//int main()
+//{
+//    string str = "abcdefg";
+//    int k = 2;
+//    Solution s;
+//    string ret = s.reverseStr(str, k);
+//    cout << ret << endl;
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
