4b107a43b9445c953f05f32cb8d510af58ba2445
GG-Bruse <BJY811@outlook.com>
2022-09-22 15:49:06 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -9575,3 +9575,83 @@ s只包含小写字母
 //	}
 //	return 0;
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+
+*/
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//class Solution
+//{
+//public:
+//    vector<int> searchRange(vector<int>& nums, int target)
+//    {
+//        if (nums.size() == 1 && nums[0] == target)return { 0,0 };
+//        vector<int> ret;
+//        int left = 0;
+//        int right = (int)nums.size() - 1;
+//        while (left <= right)
+//        {
+//            int mid = left + ((right - left) >> 1);
+//            if (nums[mid] > target)
+//            {
+//                right = mid - 1;
+//            }
+//            else if (nums[mid] < target)
+//            {
+//                left = mid + 1;
+//            }
+//            else
+//            {
+//                int flag = mid;
+//                while (flag > 0 && nums[flag] == target)
+//                {
+//                    --flag;
+//                }
+//                if (nums[flag] != target)
+//                {
+//                    ++flag;
+//                }
+//                ret.push_back(flag);
+//                flag = mid;
+//                for (; flag < nums.size() && nums[flag] == target; ++flag);
+//                ret.push_back(flag - 1);
+//                break;
+//            }
+//        }
+//        if (ret.size() == 0)
+//        {
+//            ret.push_back(-1);
+//            ret.push_back(-1);
+//        }
+//        return ret;
+//    }
+//};
+//int main()
+//{
+//    vector<int>v{ 1,4};
+//    Solution s;
+//    vector<int> ret = s.searchRange(v, 4);
+//    for (size_t i = 0; i < ret.size(); ++i)
+//    {
+//        cout << ret[i] << " ";
+//    }
+//    return 0;
+//}
