08664d7ee1a6828fa6a9c1114653cd452c2f3bad
GG-Bruse <BJY811@outlook.com>
2022-09-25 02:28:30 +0800

    C++_study

---
+++ C++文件/list.cpp
@@ -0,0 +1,239 @@
+/*
+list的介绍:
+1. list是可以在常数范围内在任意位置进行插入和删除的序列式容器，并且该容器可以前后双向迭代。
+
+2. list的底层是双向链表结构，双向链表中每个元素存储在互不相关的独立节点中，在节点中通过指针指向其前一个元素和后一个元素。
+
+3. list与forward_list非常相似：最主要的不同在于forward_list是单链表，只能朝前迭代，已让其更简单高效。
+
+4. 与其他的序列式容器相比(array，vector，deque)，list通常在任意位置进行插入、移除元素的执行效率更好。
+
+5. 与其他序列式容器相比，list和forward_list最大的缺陷是不支持任意位置的随机访问
+比如:要访问list的第6个元素，必须从已知的位置(比如头部或者尾部)迭代到该位置，在这段位置上迭代需要线性的时间开销
+list还需要一些额外的空间，以保存每个节点的相关联信息(对于存储类型较小元素的大list来说这可能是一个重要的因素)
+
+*/
+
+
+
+
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<cassert>
+namespace bjy
+{
+	template<class T>
+	struct list_node{
+		list_node(const T& data = T()): _next(nullptr),_prev(nullptr),_data(data) {}
+		list_node<T>* _next;
+		list_node<T>* _prev;
+		T _data;
+	};
+
+	template<class T,class Ref,class Ptr>
+	struct __list_iterator {
+		typedef list_node<T> list_node;
+		typedef __list_iterator<T, Ref, Ptr> iterator;
+
+		__list_iterator(list_node* node):_node(node) {}
+		bool operator!=(const iterator& it)const { return _node != it._node; }
+		bool operator==(const iterator& it)const { return _node == it._node; }
+		Ref operator*() { return _node->_data; }
+		Ptr operator->() { return &(operator*()); }
+		iterator& operator++() { 
+			_node = _node->_next;
+			return *this;
+		}
+		iterator operator++(int) {
+			iterator temp(*this);
+			_node = _node->_next;
+			return temp;
+		}
+		iterator& operator--() {
+			_node = _node->_prev;
+			return *this;
+		}
+		iterator operator--(int) {
+			iterator temp(*this);
+			_node = _node->_prev;
+			return temp;
+		}
+		list_node* _node;
+	};
+
+	template<class T>
+	class list
+	{
+		typedef T value_type;
+		typedef list_node<value_type> list_node;
+		typedef value_type& reference;
+		typedef const value_type& const_reference;
+	public:
+		typedef __list_iterator<T,T&,T*> iterator;
+		typedef __list_iterator<T, const T&, const T*> const_iterator;
+
+		iterator begin() { return iterator(_head->_next); }
+		const_iterator begin()const { return const_iterator(_head->_next); }
+		iterator end() { return iterator(_head); }
+		const_iterator end()const { return const_iterator(_head); }
+
+		list() {
+			_head = new list_node;
+			_head->_next = _head;
+			_head->_prev = _head;
+		}
+		iterator insert(iterator pos, const_reference data){
+			list_node* newnode = new list_node(data);
+			list_node* cur = pos._node;
+			list_node* prev = cur->_prev;
+
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = cur;
+			cur->_prev = newnode;
+			return iterator(newnode);
+		}
+		void push_back(const_reference data) {
+#if 0
+			list_node* new_node = new list_node(data);
+			list_node* tail = _head->_prev;
+			tail->_next = new_node;
+			new_node->_prev = tail;
+			new_node->_next = _head;
+			_head->_prev = new_node;
+#else
+			insert(end(), data);
+#endif
+		}
+		void push_front(const_reference x) { insert(begin(), x); }
+
+		iterator erase(iterator pos){
+			assert(pos != end());
+			list_node* prev = (pos._node)->_prev;
+			list_node* next = (pos._node)->_next;
+			prev->_next = next;
+			next->_prev = prev;
+			delete (pos._node);
+			return iterator(next);
+		}
+		void pop_back() { erase(--end()); }
+		void pop_front() { erase(begin()); }
+
+	private:
+		list_node* _head;
+	};
+}
+
+#include<iostream>
+#include<algorithm>
+using namespace bjy;
+using std::cout;
+using std::endl;
+using std::find;
+struct Pos
+{
+	int _x;
+	int _y;
+};
+int main1()
+{
+	list<int> l1;
+	l1.push_back(1);
+	l1.push_back(2);
+	l1.push_back(3);
+	l1.push_back(4);
+	l1.push_back(5);
+	
+	list<int>::iterator it = l1.begin();
+	for (; it != l1.end(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+	it = l1.begin();
+	for (; it != l1.end(); it++) {
+		(*it) *= 2;
+	}
+	it != l1.begin();
+	for (auto e : l1){
+		cout << e << " ";
+	}
+	cout << endl;
+
+	struct Pos pos1 {10,20};
+	struct Pos* p = &pos1;
+	cout << p->_x <<" " << p->_y << endl;//本质为p->->_x 编译器为了可读性进行了特殊处理
+	return 0;
+}
+void func(const list<int>& l)
+{
+	list<int>::const_iterator it = l.begin();
+	list<int>::const_iterator its = l.end();
+	while (it != its)
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+}
+int main2()
+{
+	list<int> l1;
+	l1.push_back(1);
+	l1.push_back(2);
+	l1.push_back(3);
+	l1.push_back(4);
+	l1.push_back(5);
+	func(l1);
+	return 0;
+}
+int main()
+{
+	list<int> lt;
+	lt.push_back(1);
+	lt.push_back(2);
+	lt.push_back(3);
+	lt.push_back(4);
+	lt.push_back(5);
+
+	list<int>::iterator it = lt.begin();
+	while (it != lt.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+
+	it = lt.begin();
+	while (it != lt.end())
+	{
+		*it *= 2;
+		++it;
+	}
+	cout << endl;
+
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	lt.push_front(10);
+	lt.push_front(20);
+	lt.push_front(30);
+	lt.push_front(40);
+
+	lt.pop_back();
+	lt.pop_back();
+
+	for (auto e : lt)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
+
--- C++文件/string.cpp
+++ C++文件/string.cpp
@@ -312,13 +312,11 @@ g++下，string是通过写时拷贝实现的，string对象总共占4个字节，内部只包含了一个指针
 //模拟实现string
 /*
 #define _CRT_SECURE_NO_WARNINGS
+#define NEW 1
 #include<iostream>
 #include<algorithm>
 #include<cstring>
 #include<cassert>
-using std::cout;
-using std::cin;
-using std::endl;
 using std::swap;
 using std::ostream;
 using std::istream;
@@ -331,122 +329,86 @@ namespace bjy
 		static size_t npos;
 		typedef char* iterator;
 		typedef const char* const_iterator;
-		iterator begin()
-		{
-			return _str;
-		}
-		iterator end()
-		{
-			return _str + _size;
-		}
-		const_iterator begin()const
-		{
-			return _str;
-		}
-		const_iterator end()const
-		{
-			return _str + _size;
-		}
+		iterator begin() { return _str; }
+		iterator end() { return _str + _size; }
+		const_iterator begin()const { return _str; }
+		const_iterator end()const { return _str + _size; }
 
+		string() :_str(new char[1]), _size(0), _capacity(0) { _str[0] = '\0'; }
 
-
-		string() :_str(new char[1]), _size(0), _capacity(0)
+#ifdef STRING_CONST_CHARPTR_STR
+		string(const char* str) : _str(new char[strlen(str) + 1]), _size(strlen(str)), _capacity(_size)//初始化顺序依赖声明顺序
 		{
-			_str[0] = '\0';
+			strcpy(_str, str);//内容拷贝
 		}
-		//string(const char* str) :_str(new char[strlen(str) + 1]), _size(strlen(str)), _capacity(_size)//初始化顺序依赖声明顺序
-		//{
-		//	strcpy(_str, str);//内容拷贝
-		//}
-		string(const char* str)
-		{
+#endif
+#ifndef STRING_CONST_CHARPTR_STR
+		string(const char* str) {
 			_size = strlen(str);
 			_capacity = _size;
 			_str = new char[_size + 1];
 			strcpy(_str, str);//内容拷贝
 		}
-		
+#endif
 
-
-		//传统写法
-		//string(const string& str)//深拷贝
-		//{
-		//	_size = str._size;
-		//	_capacity = str._capacity;
-		//	_str = new char[_capacity + 1];
-		//	strcpy(_str, str._str);
-		//}
+#ifdef OLD//深拷贝
+		string(const string& str) {
+			_size = str._size;
+			_capacity = str._capacity;
+			_str = new char[_capacity + 1];
+			strcpy(_str, str._str);
+		}
+#endif
+#ifdef NEW
 		//现代写法
-		string(const string& str):_str(nullptr),_size(0),_capacity(0)//使用初始化列表初始化，避免交换脏数据
+		string(const string& str) :_str(nullptr), _size(0), _capacity(0)//使用初始化列表初始化，避免交换脏数据
 		{
 			string strTemp(str._str);//调用构造函数
 			swap(strTemp);
 		}
+#endif
 
-
-
-		//string& operator=(const string& str)//传统写法
-		//{
-		//	if (this != &str)
-		//	{
-		//		char* pTemp = new char[strlen(str._str) + 1];
-		//		strcpy(pTemp, str._str);
-		//		delete[] _str;
-		//		_str = pTemp;
-		//		_size = str._size;
-		//		_capacity = str._capacity;
-		//	}
-		//	return *this;
-		//}
-		//string& operator=(const string& str)//现代写法1
-		//{
-		//	if (this != &str)
-		//	{
-		//		string strTemp(str._str);
-		//		swap(strTemp);
-		//	}
-		//	return *this;
-		//}
-		string& operator=(string str)//现代写法2:通过传值传参，使得str代替strTemp
-		{
+#ifdef OLD//传统写法
+		string& operator=(const string& str) {
+			if (this != &str) {
+				char* pTemp = new char[strlen(str._str) + 1];
+				strcpy(pTemp, str._str);
+				delete[] _str;
+				_str = pTemp;
+				_size = str._size;
+				_capacity = str._capacity;
+			}
+			return *this;
+		}
+#endif
+#ifdef OLDANDNEW//现代写法1
+		string& operator=(const string& str) {
+			if (this != &str) {
+				string strTemp(str._str);
+				swap(strTemp);
+			}
+			return *this;
+		}
+#endif
+#ifdef NEW//现代写法2:通过传值传参，使得str代替strTemp
+		string& operator=(string str) {
 			swap(str);
 			return *this;
 		}
-
-
-		
-		char& operator[](size_t pos)
-		{
+#endif
+		char& operator[](size_t pos) {
 			assert(pos < _size);
 			return _str[pos];
 		}
-		const char& operator[](size_t pos)const
-		{
+		const char& operator[](size_t pos)const {
 			assert(pos < _size);
 			return _str[pos];
 		}
-		bool operator>(const string& str)const
-		{
-			return strcmp(_str, str._str) > 0;
-		}
-		bool operator==(const string& str)const
-		{
-			return strcmp(_str, str._str) == 0;
-		}
-		bool operator>=(const string& str)const
-		{
-			return (*this > str || *this == str);
-		}
-		bool operator<=(const string& str)const
-		{
-			return !(*this > str);
-		}
-		bool operator<(const string& str)const
-		{
-			return !(*this >= str);
-		}
-
-		
+		bool operator>(const string& str)const { return strcmp(_str, str._str) > 0; }
+		bool operator==(const string& str)const { return strcmp(_str, str._str) == 0; }
+		bool operator>=(const string& str)const { return (*this > str || *this == str); }
+		bool operator<=(const string& str)const { return !(*this > str); }
+		bool operator<(const string& str)const { return !(*this >= str); }
 
 		string& insert(size_t pos, char ch)
 		{
@@ -477,7 +439,7 @@ namespace bjy
 		}
 		void push_back(char ch)
 		{
-			insert(_size,ch);
+			insert(_size, ch);
 		}
 		void append(size_t num, char ch)
 		{
@@ -487,36 +449,76 @@ namespace bjy
 				push_back(ch);
 			}
 		}
-		void append(const char* str)
-		{
-			insert(_size, str);
-		}
-		void append(const string& str)
-		{
-			append(str._str);
-		}
-		string& operator+=(char ch)
-		{
+		void append(const char* str) { insert(_size, str); }
+		void append(const string& str) { append(str._str); }
+		string& operator+=(char ch) {
 			push_back(ch);
 			return *this;
 		}
-		string& operator+=(const char* str)
-		{
+		string& operator+=(const char* str) {
 			append(str);
 			return *this;
 		}
-		string& operator+=(const string& str)
-		{
+		string& operator+=(const string& str) {
 			append(str._str);
 			return *this;
 		}
 
+		const char* c_str()const { return _str; }
+		size_t size()const { return _size; }
 
-
-		void reserve(size_t num)
-		{
-			if (num > _capacity)
-			{
+		void erase(size_t pos, size_t lenth = npos) {
+			assert(pos < _size);
+			if (lenth == npos || pos + lenth >= _size) {//需要删完
+				_str[pos] = '\0';
+				_size = pos;
+			}
+			else {
+				strcpy(_str + pos, _str + pos + lenth);
+				_size -= lenth;
+			}
+		}
+		void clear() {
+			_str[0] = '\0';
+			_size = 0;
+		}
+		void resize(size_t n, char ch = '\0') {
+			if (n > _size) {
+				reserve(n);
+				for (size_t i = _size; i < n; ++i) {
+					_str[i] = ch;
+				}
+				_str[n] = '\0';
+				_size = n;
+			}
+			else {
+				_str[n] = '\0';
+				_size = n;
+			}
+		}
+		size_t find(char ch, size_t pos = 0)const {
+			for (size_t i = pos; i < _size; ++i) {
+				if (ch == _str[i]) return i;
+			}
+			return npos;
+		}
+		size_t find(const char* sub, size_t pos = 0) {
+			assert(sub);
+			assert(pos < _size);
+			const char* ptr = strstr(_str + pos, sub);
+			if (ptr == nullptr) return npos;
+			return ptr - _str;
+		}
+		string substr(size_t pos = 0, size_t len = npos)const {
+			assert(pos < _size);
+			size_t realLenth = len;
+			if (len == npos || len + len > _size) realLenth = _size - pos;
+			string strTemp;
+			for (size_t i = 0; i < realLenth; ++i) strTemp += _str[pos + i];
+			return strTemp;
+		}
+		void reserve(size_t num) {
+			if (num > _capacity) {
 				char* temp = new char[num + 1];
 				strcpy(temp, _str);
 				delete[] _str;
@@ -524,96 +526,14 @@ namespace bjy
 				_capacity = num;
 			}
 		}
-		const char* c_str()const
-		{
-			return _str;
-		}
-		size_t size()const
-		{
-			return _size;
-		}
-		void erase(size_t pos ,size_t lenth = npos)
-		{
-			assert(pos < _size);
-			if (lenth == npos || pos + lenth >= _size)//需要删完
-			{
-				_str[pos] = '\0';
-				_size = pos;
-			}
-			else
-			{
-				strcpy(_str + pos, _str + pos + lenth);
-				_size -= lenth;
-			}
-		}
-		void clear()
-		{
-			_str[0] = '\0';
-			_size = 0;
-		}
-		void resize(size_t n, char ch = '\0')
-		{
-			if (n > _size)
-			{
-				reserve(n);
-				for (size_t i = _size; i < n; ++i)
-				{
-					_str[i] = ch;
-				}
-				_str[n] = '\0';
-				_size = n;
-			}
-			else
-			{
-				_str[n] = '\0';
-				_size = n;
-			}
-		}
-		size_t find(char ch, size_t pos = 0)const
-		{
-			for (size_t i = pos; i < _size; ++i)
-			{
-				if (ch == _str[i])
-				{ 
-					return i;
-				}
-			}
-			return npos;
-		}
-		size_t find(const char* sub, size_t pos = 0)
-		{
-			assert(sub);
-			assert(pos < _size);
-			const char* ptr = strstr(_str + pos, sub);
-			if (ptr == nullptr)
-				return npos;
-			return ptr - _str;
-		}
-		string substr(size_t pos = 0, size_t len = npos)const
-		{
-			assert(pos < _size);
-			size_t realLenth = len;
-			if (len == npos || len + len > _size)
-			{
-				realLenth = _size - pos;
-			}
-			string strTemp;
-			for (size_t i = 0; i < realLenth; ++i)
-			{
-				strTemp += _str[pos + i];
-			}
-			return strTemp;
-		}
-		
-		~string()
-		{
+
+		~string() {
 			delete[] _str;
 			_str = nullptr;
 			_capacity = _size = 0;
 		}
 	private:
-		void swap(string& strTemp)
-		{
+		void swap(string& strTemp) {
 			::swap(_str, strTemp._str);
 			::swap(_size, strTemp._size);
 			::swap(_capacity, strTemp._capacity);
@@ -623,24 +543,19 @@ namespace bjy
 		size_t _size;
 		size_t _capacity;
 	};
-
 	size_t string::npos = -1;
-	ostream& operator<<(ostream& out, const string& str)
-	{
+	ostream& operator<<(ostream& out, const string& str) {
 		out << str.c_str();
 		return out;
 	}
-	istream& operator>>(istream& in, string& str)
-	{
+	istream& operator>>(istream& in, string& str) {
 		str.clear();
 		const size_t N = 32;
 		char buff[N];
 		size_t i = 0;
-		for(char chTemp = in.get();chTemp != ' ' && chTemp != '\n'; chTemp = in.get())
-		{
+		for (char chTemp = in.get(); chTemp != ' ' && chTemp != '\n'; chTemp = in.get()) {
 			buff[i++] = chTemp;
-			if (i == N - 1)
-			{
+			if (i == N - 1) {
 				buff[i] = '\0';
 				str += buff;
 				i = 0;
@@ -653,7 +568,10 @@ namespace bjy
 }
 
 
-
+using std::cout;
+using std::cin;
+using std::endl;
+using std::find;
 using namespace bjy;
 int main()
 {
@@ -731,69 +649,9 @@ int main()
 
 	string str6("hello");
 	string str7;
-	cin >> str6>>str7;
-	cout << str6 <<str7<< endl;
+	cin >> str6 >> str7;
+	cout << str6 << str7 << endl;
 
 	return 0;
 }
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
