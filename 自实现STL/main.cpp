494ad09cd66599a2afd1b7a6bef5b92e00fe8cb1
GG-Bruse <BJY811@outlook.com>
2022-09-24 16:00:52 +0800

    Self_realization_STL

--- 自实现STL/main.cpp
+++ 自实现STL/main.cpp
@@ -1,6 +1,12 @@
 #include"string.h"
+#include"vector.hpp"
+#include<algorithm>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::find;
 using namespace bjy;
-int main()
+int main_string()
 {
 	string str1("hahaha");
 	cout << str1.c_str() << endl;
@@ -79,5 +85,134 @@ int main()
 	cin >> str6 >> str7;
 	cout << str6 << str7 << endl;
 
+	return 0;
+}
+
+
+
+
+
+//main_vector
+int main1()
+{
+	vector<int>v;
+	for (size_t i = 0; i < 10; ++i)
+	{
+		v.push_back(i + 1);
+	}
+
+	vector<int>::iterator it = find(v.begin(), v.end(), 5);
+	v.insert(it, 30);
+	for (auto& e : v)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	for (size_t i = 0; i < 10; ++i)
+	{
+		v.push_back(i + 1);
+	}
+
+	it = find(v.begin(), v.end(), 5);
+	v.erase(it);
+	for (auto& e : v)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+
+	for (vector<int>::iterator it = v.begin(); it != v.end();) {
+		if (*it % 2 == 0)
+		{
+			it = v.erase(it);
+		}
+		else {
+			++it;
+		}
+	}
+	for (auto e : v)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+	return 0;
+}
+int main2()
+{
+	vector<int>v;
+	for (size_t i = 0; i < 10; ++i)
+	{
+		v.push_back(i);
+	}
+	vector<int>v1(v);
+	for (size_t i = 0; i < v1.size(); ++i)
+	{
+		cout << v1[i] << " ";
+	}
+
+	vector<int>v2;
+	v2 = v1;
+	v1[0] *= 10;
+	for (size_t i = 0; i < v2.size(); ++i)
+	{
+		cout << v2[i] << " ";
+	}
+
+	return 0;
+}
+int main3()
+{
+	vector<int>v(10, 1);//调用vector(size_t n, const_reference val = T())函数
+	//C++中内置类型升级，存在构造函数
+	for (size_t i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << " ";
+	}
+	return 0;
+}
+int main4()
+{
+	vector<int>v;
+	v.resize(10);
+	for (size_t i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	for (size_t i = 1; i <= 100; ++i)
+		v.push_back(i);
+	v.resize(20);
+	for (size_t i = 0; i < v.size(); ++i)
+		cout << v[i] << " ";
+	return 0;
+}
+
+class Solution
+{
+public:
+	vector<vector<int>> generate(int numRows)
+	{
+		static vector<vector<int>> ret(numRows);
+		for (int i = 0; i < numRows; ++i)
+		{
+			ret[i].resize(i + 1);//每行大小设置
+			ret[i][0] = ret[i][i] = 1;//每行第一个和最后一个元素都为1，其他元素也暂时赋值为1
+			for (int j = 1; j < i; ++j)
+			{
+				ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];//元素的值等于 上一行该位置的值 加 上一行对应位置前一个的值
+			}
+		}
+		return ret;
+	}
+};
+int main5()
+{
+	vector<vector<int>>ret = Solution().generate(5);
+	for (size_t i = 0; i < ret.size(); ++i)
+	{
+		for (size_t j = 0; j < ret[i].size(); ++j)
+		{
+			cout << ret[i][j] << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
---
+++ 自实现STL/vector.hpp
@@ -0,0 +1,140 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define NEW 1
+#include<iostream>
+#include<algorithm>
+#include<cassert>
+using std::swap;
+namespace bjy
+{
+	template<class T>
+	class vector
+	{
+	public:
+		typedef T value_type;
+		typedef value_type* iterator;
+		typedef const value_type* const_iterator;
+		typedef value_type& reference;
+		typedef const value_type& const_reference;
+	public:
+		iterator begin() { return _start; }
+		iterator end() { return _finish; }
+		const_iterator begin()const { return _start; }
+		const_iterator end()const { return _finish; }
+
+		vector() :_start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {}
+#ifdef OLD1
+		vector(const vector<int>& temp) {
+			_start = new T[temp.capacity()];
+			//memcpy(_start, temp._start,sizeof(T)*temp.size());//自定义类型不可使用memcpy
+			for (size_t i = 0; i < temp.size(); ++i) _start[i] = temp._start[i];
+			_finish = _start + temp.size();
+			_end_of_storage = _start + temp.capacity();
+		}
+#endif
+#ifdef OLD2
+		vector(const vector<int>& temp) :_start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {
+			reserve(temp.capacity());
+			for (size_t i = 0; i < temp.size(); ++i)
+				push_back(temp[i]);
+		}
+#endif
+#ifdef NEW//现代写法
+		template<class InputIterator>
+		vector(InputIterator first, InputIterator last) {
+			for (; first != last; ++first) push_back(*first);
+		}
+		vector(const vector<int>& temp) :_start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {
+			vector<int>worker(temp.begin(), temp.end());
+			swap(worker);
+		}
+#endif
+		vector(int n, const_reference val = T()) {
+			reserve(n);
+			for (int i = 0; i < n; ++i) push_back(val);
+		}
+		vector(size_t n, const_reference val = T()) {
+			reserve(n);
+			for (size_t i = 0; i < n; ++i) push_back(val);
+		}
+		~vector() {
+			delete[] _start;
+			_start = _finish = _end_of_storage = nullptr;
+		}
+
+		size_t capacity()const { return _end_of_storage - _start; }
+		size_t size()const { return _finish - _start; }
+
+		reference operator[](size_t pos) {
+			assert(pos < size());
+			return _start[pos];
+		}
+		const_reference& operator[](size_t pos)const {
+			assert(pos < size());
+			return _start[pos];
+		}
+		vector<T>& operator=(vector<T> temp) {
+			swap(temp);
+			return *this;
+		}
+		void swap(vector<T>& temp) {
+			std::swap(_start, temp._start);
+			std::swap(_finish, temp._finish);
+			std::swap(_end_of_storage, temp._end_of_storage);
+		}
+
+		void reserve(size_t n) {
+			size_t previous_size = size();
+			if (n > capacity()) {
+				value_type* temp = new value_type[n];
+				if (_start) {
+					for (size_t i = 0; i < previous_size; ++i) temp[i] = _start[i];
+					delete[] _start;
+				}
+				_start = temp;
+				_finish = _start + previous_size;
+				_end_of_storage = _start + n;
+			}
+		}
+		void resize(size_t n, const_reference val = T()) {
+			if (n > capacity())reserve(n);
+			if (n > size())
+				while (_finish < _end_of_storage) *_finish++ = val;
+			else
+				_finish = _start + n;
+		}
+		iterator insert(iterator pos, const_reference data) {
+			assert((pos >= _start) && (pos <= _finish));
+			if (_finish >= _end_of_storage) {
+				size_t lenth = pos - _start;//记录,避免迭代器失效
+				reserve(capacity() == 0 ? 6 : capacity() * 2);
+				pos = _start + lenth;//更新
+			}
+			for (iterator cur = _finish - 1; cur >= pos; --cur)
+				*(cur + 1) = *cur;
+			*pos = data;
+			++_finish;
+			return pos;
+		}
+		iterator erase(iterator pos) {
+			assert((pos >= _start) && (pos < _finish));
+			for (iterator cur = pos + 1; cur < _finish; ++cur)
+				*(cur - 1) = *cur;
+			--_finish;
+			return pos;
+		}
+		void push_back(const_reference data) {
+			if (_finish >= _end_of_storage)
+				reserve(capacity() == 0 ? 6 : capacity() * 2);
+			*(_finish++) = data;
+		}
+		void pop_back() {
+			assert(_finish > _start);
+			--_finish;
+		}
+	private:
+		iterator _start;
+		iterator _finish;
+		iterator _end_of_storage;
+	};
+}
