50f0c58d53d628175da2dc5a57b34f1d5518c80b
GG-Bruse <BJY811@outlook.com>
2022-09-28 06:02:06 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -9973,3 +9973,107 @@ s只包含小写字母
 //	cout << endl;
 //	return 0;
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+实现 MinStack 类:
+MinStack() 初始化堆栈对象。
+void push(int val) 将元素val推入堆栈。
+void pop() 删除堆栈顶部的元素。
+int top() 获取堆栈顶部的元素。
+int getMin() 获取堆栈中的最小元素
+*/
+
+//class MinStack {
+//public:
+//    MinStack() {}
+//    void push(int val) {
+//        _stValue.push(val);
+//        if (_stMinValue.empty() || val <= _stMinValue.top())
+//            _stMinValue.push(val);
+//    }
+//    void pop() {
+//        if (_stValue.top() == _stMinValue.top())
+//            _stMinValue.pop();
+//        _stValue.pop();
+//    }
+//    int top() { return _stValue.top(); }
+//    int getMin() { return _stMinValue.top(); }
+//private:
+//    stack<int> _stValue;
+//    stack<int> _stMinValue;
+//};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+
+
+
+
+
+
+
+
+/*
+输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。假设压入栈的所有数字均不相等。
+例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+1. 0<=pushV.length == popV.length <=1000
+2. -1000<=pushV[i]<=1000
+3. pushV 的所有数字均不相同
+*/
+//#include<iostream>
+//#include<vector>
+//#include<stack>
+//using namespace std;
+//class Solution
+//{
+//public:
+//    bool IsPopOrder(vector<int> pushV, vector<int> popV)
+//    {
+//        stack<int> st;
+//        int popi = 0;
+//        for (auto pushVal : pushV)
+//        {
+//            st.push(pushVal);
+//            while (!st.empty() && popV[popi] == st.top())
+//            {
+//                ++popi;
+//                st.pop();
+//            }
+//        }
+//        //return popi == popV.size();
+//        return st.empty();
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+
