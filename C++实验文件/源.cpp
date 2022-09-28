747d451cc3c1b55ecd7199d04f5f5012b7ad86f8
GG-Bruse <BJY811@outlook.com>
2022-09-28 18:17:32 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -10073,7 +10073,49 @@ int getMin() 获取堆栈中的最小元素
 
 
 
+/*
+根据 逆波兰表示法，求表达式的值。
 
+有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
 
+注意 两个整数之间的除法只保留整数部分。
+
+可以保证给定的逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况
+*/
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//#include<stack>
+//using namespace std;
+//class Solution
+//{
+//public:
+//    int evalRPN(vector<string>& tokens)
+//    {
+//        stack<long long> st;
+//        for (size_t i = 0; i < tokens.size(); ++i)
+//        {
+//            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+//            {
+//                long long right = st.top();
+//                st.pop();
+//                long long left = st.top();
+//                st.pop();
+//                switch (tokens[i][0])
+//                {
+//                case '+':st.push(left + right); break;
+//                case '-':st.push(left - right); break;
+//                case '*':st.push(left * right); break;
+//                case '/':st.push(left / right); break;
+//                }
+//            }
+//            else
+//            {
+//                st.push(stoll(tokens[i]));
+//            }
+//        }
+//        return st.top();
+//    }
+//};
 
 
