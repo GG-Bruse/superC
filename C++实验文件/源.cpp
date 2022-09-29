45c40eec6994684846c6806f89e8a6648cea9a01
GG-Bruse <BJY811@outlook.com>
2022-09-29 22:13:39 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -10119,3 +10119,36 @@ int getMin() 获取堆栈中的最小元素
 //};
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+你必须设计并实现时间复杂度为 O(n) 的算法解决此问题
+*/
+//#include<iostream>
+//#include<vector>
+//#include<queue>
+//using namespace std;
+//class Solution
+//{
+//public:
+//    int findKthLargest(vector<int>& nums, int k)
+//    {
+//        priority_queue<int> heap(nums.begin(), nums.end());
+//        for (int i = 0; i < k - 1; ++i) { heap.pop(); }
+//        return heap.top();
+//    }
+//};
