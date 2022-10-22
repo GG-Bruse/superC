c85dbc3c3a9adaf72ee2c28edc43bd96728f2eb9
GG-Bruse <BJY811@outlook.com>
2022-10-23 00:09:15 +0800

    C++_study_plus

--- C++文件/多态.cpp
+++ C++文件/多态.cpp
@@ -20,8 +20,7 @@
 即基类虚函数返回基类对象(不一定是本类，其他继承关系中的基类也行)的指针或者引用，
 派生类虚函数返回派生类对象(不一定是本类，其他继承关系中的派生类也行)的指针或者引用时，称为协变
 
-3.析构函数的重写(基类与派生类析构函数的名字不同)
-若基类的析构函数为虚函数，此时派生类析构函数只要定义，无论是否加virtual关键字，都与基类的析构函数构成重写。
+3.析构函数的重写(基类与派生类析构函数的名字不同) 建议将析构函数定义成虚函数
 虽然函数名不相同，但这里编译器对析构函数的名称做了特殊处理，编译后析构函数的名称统一处理成destructor
 
 
@@ -32,8 +31,21 @@
 */
 
 
+// C++11 override和final
+/*
+final:		修饰虚函数，表示该虚函数不能再被重写
+override:	检查派生类虚函数是否重写了基类某个虚函数，如果没有重写编译报错
+*/
 
 
+
+//抽象类
+/*
+在虚函数的后面写上 = 0 ，则这个函数为纯虚函数。
+包含纯虚函数的类叫做抽象类（也叫接口类），抽象类不能实例化出对象
+派生类继承后也不能实例化出对象，只有重写纯虚函数，派生类才能实例化出对象
+纯虚函数规范了派生类必须重写，另外纯虚函数更体现出了接口继承
+*/
 
 //#include<iostream>
 //class A
@@ -57,6 +69,153 @@
 普通函数继承是实现继承
 虚函数重写是接口继承(即成员函数框架继承)、重写实现
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//多态的原理
+
+/*
+除了_b成员，还多一个__vfptr放在对象的前面(注意有些平台可能会放到对象的最后面，这个跟平台有关)
+对象中的这个指针我们叫做虚函数表指针(v代表virtual，f代表function)。
+一个含有虚函数的类中都至少都有一个虚函数表指针，因为虚函数的地址要被放到虚函数表中，虚函数表也简称虚表。
+*/
+//#include<iostream>
+//using namespace std;
+//class Base
+//{
+//public:
+//	virtual void Func1()
+//	{
+//		cout << "Func1()" << endl;
+//	}
+//private:
+//	int _b = 1;
+//};
+//int main()
+//{
+//	cout << sizeof(Base) << endl;//8
+//	return 0;
+//}
+
+
+#include<iostream>
+using namespace std;
+class Base
+{
+public:
+	virtual void Func1()
+	{
+		cout << "Base::Func1()" << endl;
+	}
+	virtual void Func2()
+	{
+		cout << "Base::Func2()" << endl;
+	}
+	void Func3()
+	{
+		cout << "Base::Func3()" << endl;
+	}
+private:
+	int _b = 1;
+};
+class Derive : public Base
+{
+public:
+	virtual void Func1()
+	{
+		cout << "Derive::Func1()" << endl;
+	}
+private:
+	int _d = 2;
+};
+int main()
+{
+	Base b;
+	Derive d;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
