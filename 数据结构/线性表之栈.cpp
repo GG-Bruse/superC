cfd13097b8cc72699ce15c828135b0f32ac67e6b
GG-Bruse <BJY811@outlook.com>
2022-11-05 19:52:47 +0800

    data_structure

--- 数据结构/线性表之栈.cpp
+++ 数据结构/线性表之栈.cpp
@@ -10,387 +10,100 @@
 
 /*
 利用数组和链表都可实现栈
-1.数组:(更优) 
+1.数组:(更优、访问效率更高) 
 数组尾当作栈顶，数组头作栈底。栈顶需频繁插入删除，使用数组尾可避免数据挪动。
 缺陷:栈空间不足时的扩容及其扩容后空间的浪费
 2.链表:
 (单链表)链表头作栈顶，链表尾作栈底。栈顶需频繁插入删除，使用链表头作栈顶可避免频繁遍历寻找尾节点。
-(双向循环链表)链表头、链表尾作栈顶都可 
+(双向循环链表)链表头、链表尾作栈顶都可
 */
 
 
 
-//C语言利用动态数组实现栈(1) 
-/*
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<assert.h>
-typedef int SKDataType; 
-typedef struct stack
-{
-	SKDataType* data;
-	int top;
-	int capacity;
-}Stack;
-void StackInit(Stack* ps)
-{
-	assert(ps);
-	ps->data = (SKDataType*)malloc(sizeof(SKDataType)*4);
-	if(ps->data == NULL)
-	{
-		printf("malloc fail\n");
-		exit(-1);
-	}
-	 
-	ps->capacity = 4;
-	ps->top = 0;//top指向栈顶元素的上一个位置 
-}
-void StackDestory(Stack* ps)
-{
-	assert(ps);
-	free(ps->data);
-	ps->data = NULL;
-	ps->capacity = ps->top = 0;
-}
-void StackPush(Stack* ps,SKDataType d)
-{
-	assert(ps);
-	if(ps->top == ps->capacity)
-	{
-		SKDataType* newSpace = (SKDataType*)realloc(ps->data,ps->capacity * 2 * sizeof(SKDataType));
-		if(newSpace == NULL)
-		{
-			printf("realloc fail\n");
-			exit(-1);
-		}
-		ps->data = newSpace;
-		ps->capacity *= 2;
-	}
-	ps->data[ps->top] = d;
-	++ps->top; 
-}
-void StackPop(Stack* ps)
-{
-	assert(ps);
-	assert(ps->top > 0);//栈不可为空 
-	--ps->top;
-}
-SKDataType StackTop(Stack* ps)
-{
-	assert(ps);
-	assert(ps->top > 0);
-	return ps->data[ps->top - 1];
-}
-int StackSize(Stack* ps)
-{
-	assert(ps);
-	return ps->top;
-}
-bool StackIsEmpty(Stack* ps) 
-{
-	assert(ps);
-	return ps->top == 0;
-} 
-int main()
-{
-	Stack sk;
-	StackInit(&sk);
-	StackPush(&sk,1);
-	StackPush(&sk,2);
-	StackPush(&sk,3);
-	StackPush(&sk,4);
-	StackPush(&sk,5);
-	
-	while(!StackIsEmpty(&sk))
-	{
-		printf("%d ",StackTop(&sk));
-		StackPop(&sk);
-	}
-	
-	StackDestory(&sk);
-	return 0; 
-} 
-*/
-
-
-
-
-
-
-
-//C++利用数组实现栈 
-/*
-#define MAX  1024
-#include<iostream>
-#include<cstring>
-using namespace std;
-
-class Stack
-{
-public:
-	Stack();
-	void top_back(void* data);//压栈
-	void pop_back();//出栈
-	void* top();//返回栈顶
-	int size();
-	bool isEmpty();
-	~Stack();
-private:
-	void* data[MAX];//指针数组——栈数组
-	int m_size;
-};
-Stack::Stack()
-{
-	this->m_size = 0;
-	memset(this->data, 0, sizeof(void*) * MAX);
-}
-
-void Stack::top_back(void* data)
-{
-	if (data == nullptr)return;
-	this->data[this->m_size] = data;
-	++this->m_size;
-}
-
-void Stack::pop_back()
-{
-	this->data[this->m_size - 1] = nullptr;
-	--this->m_size;
-}
-
-void* Stack::top()
-{
-	if (this->m_size == 0)return nullptr;
-	return this->data[this->m_size - 1];
-}
-
-int Stack::size()
-{
-	return this->m_size;
-}
-
-bool Stack::isEmpty()
-{
-	if (this->m_size == 0)return true;
-	return false;
-}
-
-Stack::~Stack()
-{
-	this->m_size = 0;
-	memset(this->data, 0, sizeof(void*) * MAX);
-}
-bool isLeft(char ch)
-{
-	return ch == '(';
-}
-bool isRight(char ch)
-{
-	return ch == ')';
-}
-void printError(char* str, string errMsg, char* pos)
-{
-	cout << "错误信息:" << errMsg << endl;
-	cout << str << endl;
-	int num = pos - str;
-	for (int i = 0; i < num; ++i)
-	{
-		cout << " ";
-	}
-	cout << "~" << endl;
-}
-int main()
-{
-	char* str = (char*)"5 + 5 * (6) + 9 / 3 * 1 - ( 1 + 310";
-	char* p = str;
-	Stack sk;
-	while (*p != '\0')
-	{
-		if (isLeft(*p))
-		{
-			sk.top_back(p);
-		}
-		if (isRight(*p))
-		{
-			if (sk.size() > 0)
-			{
-				sk.pop_back();
-			}
-			else
-			{
-				printError(str, "右括号没有匹配到对应的左括号", p);
-			}
-		}
-		++p;
-	}
-	while (sk.size() > 0)
-	{
-		printError(str, "左括号没有匹配到右括号", (char*)sk.top());
-		sk.pop_back();
-	}
-	return 0;
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//C++利用单链表实现栈
-/*
-#include<iostream>
-#include<cstdlib>
-using namespace std;
-class StackNode
-{
-	friend class StackLink;
-private:
-	StackNode* next;
-	void* data;
-};
-
-class StackLink
-{
-public:
-	StackLink();
-	void top_back(void* data);
-	void pop_back();
-	void* top();
-	int size();
-	bool isEmpty();
-	~StackLink();
-private:
-	StackNode pHeader;
-	int m_size;
-};
-StackLink::StackLink()
-{
-	this->pHeader.next = nullptr;
-	this->m_size = 0;
-}
-
-void StackLink::top_back(void* data)
-{
-	if (data == nullptr)return;
-	StackNode* myNode = (StackNode*)malloc(sizeof(StackNode));
-	if(myNode == nullptr)return;
-	myNode->data = data;
-	
-	myNode->next = this->pHeader.next;
-	this->pHeader.next = myNode;
-	++this->m_size;
-}
-
-void StackLink::pop_back()
-{
-	StackNode* pDel = this->pHeader.next;
-	this->pHeader.next = pDel->next;
-	free(pDel);
-	pDel = nullptr;
-	--this->m_size;
-}
-
-void* StackLink::top()
-{
-	if (this->m_size == 0)return nullptr;
-	return this->pHeader.next->data;
-}
-
-int StackLink::size()
-{
-	return this->m_size;
-}
-
-bool StackLink::isEmpty()
-{
-	if (this->m_size == 0)
-	{
-		return true;
-	}
-	return false;
-}
-
-StackLink::~StackLink()
-{
-	for(StackNode* cur = pHeader.next;cur != nullptr;)
-	{
-		StackNode* next = cur->next;
-		free(cur);
-		cur = next;
-	}
-	this->pHeader.next = nullptr;
-	this->m_size = 0;
-}
-bool isLeft(char ch)
-{
-	return ch == '(';
-}
-bool isRight(char ch)
-{
-	return ch == ')';
-}
-void printError(char* str, string errMsg, char* pos)
-{
-	cout << "错误信息:" << errMsg << endl;
-	cout << str << endl;
-	int num = pos - str;
-	for (int i = 0; i < num; ++i)
-	{
-		cout << " ";
-	}
-	cout << "~" << endl;
-}
-int main()
-{
-	char* str = (char*)"5 + 5 * (6) + 9 / 3 * 1 - ) 1 + 310";
-	char* p = str;
-	StackLink sk;
-	while (*p != '\0')
-	{
-		if (isLeft(*p))
-		{
-			sk.top_back(p);
-		}
-		if (isRight(*p))
-		{
-			if (sk.size() > 0)
-			{
-				sk.pop_back();
-			}
-			else
-			{
-				printError(str, "右括号没有匹配到对应的左括号", p);
-			}
-		}
-		++p;
-	}
-	while (sk.size() > 0)
-	{
-		printError(str, "左括号没有匹配到右括号", (char*)sk.top());
-		sk.pop_back();
-	}
-	return 0;
-}
-*/
-
-
-
-
-
-
-
-
+//C语言利用动态数组实现栈
+//#include<stdio.h>
+//#include<stdbool.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//typedef int SKDataType; 
+//typedef struct stack
+//{
+//	SKDataType* data;
+//	int top;//指向最后一个数据的下一个位置
+//	int capacity;
+//}Stack;
+//void StackInit(Stack* ps)
+//{
+//	assert(ps);
+//	ps->data = NULL;
+//	ps->capacity = ps->top = 0;
+//}
+//void StackDestory(Stack* ps)
+//{
+//	assert(ps);
+//	free(ps->data);
+//	ps->data = NULL;
+//	ps->capacity = ps->top = 0;
+//}
+//void StackPush(Stack* ps,SKDataType d)
+//{
+//	assert(ps);
+//	if(ps->top == ps->capacity)
+//	{
+//		int newCapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+//		SKDataType* newSpace = (SKDataType*)realloc(ps->data,newCapacity * sizeof(SKDataType));
+//		if(newSpace == NULL)
+//		{
+//			printf("realloc fail\n");
+//			exit(-1);
+//		}
+//		ps->data = newSpace;
+//		ps->capacity = newCapacity;
+//	}
+//	ps->data[ps->top] = d;
+//	++ps->top;
+//}
+//void StackPop(Stack* ps)
+//{
+//	assert(ps);
+//	assert(ps->top > 0);//栈不可为空 
+//	--ps->top;
+//}
+//SKDataType StackTop(Stack* ps)
+//{
+//	assert(ps);
+//	assert(ps->top > 0);
+//	return ps->data[ps->top - 1];
+//}
+//int StackSize(Stack* ps)
+//{
+//	assert(ps);
+//	return ps->top;
+//}
+//bool StackIsEmpty(Stack* ps) 
+//{
+//	assert(ps);
+//	return ps->top == 0;
+//} 
+//int main()
+//{
+//	Stack sk;
+//	StackInit(&sk);
+//	StackPush(&sk,1);
+//	StackPush(&sk,2);
+//	StackPush(&sk,3);
+//	StackPush(&sk,4);
+//	StackPush(&sk,5);
+//	
+//	while(!StackIsEmpty(&sk))
+//	{
+//		printf("%d ",StackTop(&sk));
+//		StackPop(&sk);
+//	}
+//	
+//	StackDestory(&sk);
+//	return 0; 
+//} 
 
 
 
--- 数据结构/线性表之链表.cpp
+++ 数据结构/线性表之链表.cpp
@@ -13,7 +13,8 @@
 1.无头单向非循环链表:
 结构简单，一般不会单独用来存储数据。实践应用中一般作为其他数据结构的子结构，如哈希桶、图的邻接表等。
 2.带头双向循环链表:
-结构最复杂，一般用于单独存储数据。实际中使用的链表都为该种链表。 
+结构最复杂，一般用于单独存储数据。实际中使用的链表都为该种链表
+最优的链表结构，在任意位置的插入删除(不包括插入删除结点的查找)都为O(1)
 */ 
 
 
@@ -23,175 +24,170 @@
 
 
 //无头单向非循环链表的C语言实现
-/*
-#include<stdio.h>
-#include<stdlib.h>
-typedef int SLDataType;
-typedef struct SListNode
-{
-	SLDataType data;
-	struct SListNode* next;
-}SLNode;
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//typedef int SLDataType;
+//typedef struct SListNode
+//{
+//	SLDataType data;
+//	struct SListNode* next;
+//}SLNode;
+//
+//SLNode* BuyListNode(SLDataType d)//创建一个结点 
+//{
+//	SLNode* newNode = (SLNode*)malloc(sizeof(SLNode));
+//	if (newNode == NULL)
+//		return NULL;
+//	newNode->data = d;
+//	newNode->next = NULL;
+//	return newNode;
+//}
+//void SListPushFront(SLNode** ppheader, SLDataType d)//头插 
+//{
+//	assert(ppheader);
+//	SLNode* newNode = BuyListNode(d);
+//	newNode->next = *ppheader;
+//	*ppheader = newNode;
+//}
+//
+//void SListPushBack(SLNode** ppheader, SLDataType d)//尾插 
+//{
+//	assert(ppheader);
+//	SLNode* newNode = BuyListNode(d);
+//
+//	if (*ppheader == NULL)
+//	{
+//		*ppheader = newNode;
+//	}
+//	else
+//	{
+//		SLNode* tail = *ppheader;
+//		while (tail->next != NULL)
+//		{
+//			tail = tail->next;
+//		}
+//		tail->next = newNode;
+//	}
+//}
+//
+//void SListPopFront(SLNode** ppheader)//头删 
+//{
+//	if (*ppheader == NULL)
+//		return;
+//	SLNode* temp = *ppheader;
+//	*ppheader = (*ppheader)->next;
+//	free(temp);
+//	temp = NULL;
+//}
+//
+//void SListPopBack(SLNode** ppheader)//尾删 
+//{
+//	if (*ppheader == NULL)
+//	{
+//		return;
+//	}
+//	else if ((*ppheader)->next == NULL)
+//	{
+//		free(*ppheader);
+//		*ppheader = NULL;
+//		return;
+//	}
+//
+//	SLNode* prev = NULL;
+//	SLNode* tail = *ppheader;
+//	for (; tail->next != NULL; tail = tail->next)
+//	{
+//		prev = tail;
+//	}
+//	free(tail);
+//	prev->next = NULL;
+//}
+//void SListInsert(SLNode** ppheader, SLNode* pos, SLDataType d)//在pos的前面插入 
+//{
+//	assert(ppheader && pos);
+//	if (pos == *ppheader)
+//	{
+//		SListPushFront(ppheader, d);
+//	}
+//	else
+//	{
+//		SLNode* newNode = BuyListNode(d);
+//		SLNode* prev = *ppheader;
+//		for (; prev->next != pos; prev = prev->next);
+//		prev->next = newNode;
+//		newNode->next = pos;
+//	}
+//}
+//void SListErase(SLNode** ppheader, SLNode* pos)//删除pos位置的结点 
+//{
+//	assert(ppheader && pos);
+//	if (pos == *ppheader)
+//	{
+//		SListPopFront(ppheader);
+//	}
+//	else
+//	{
+//		SLNode* prev = *ppheader;
+//		for (; prev->next != pos; prev = prev->next);
+//		prev->next = pos->next;
+//		free(pos);
+//		pos = NULL;
+//	}
+//}
+//SLNode* SListFind(SLNode* pheader,SLDataType d)
+//{
+//	SLNode* cur = pheader;
+//	for(;cur != NULL;cur = cur->next)
+//	{
+//		if(cur->data == d)
+//		{
+//			return cur;
+//		}
+//	}
+//	return NULL;
+//}
+//void SListPrint(SLNode* pheader)
+//{
+//	for (SLNode* cur = pheader; cur != NULL; cur = cur->next)
+//	{
+//		printf("%d ", cur->data);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	SLNode* pheader = NULL;
+//	SListPushBack(&pheader, 1);
+//	SListPushBack(&pheader, 2);
+//	SListPushBack(&pheader, 3);
+//	SListPushBack(&pheader, 4);
+//	SListPushFront(&pheader,0);
+//
+//	SListPrint(pheader);
+//	
+//	//在3前面插入30
+//	SLNode* pos = SListFind(pheader,3);
+//	if(pos)
+//		SListInsert(&pheader,pos,30); 
+//	
+//	
+//	pos = SListFind(pheader,0);
+//	if(pos)
+//		SListErase(&pheader,pos);
+//	
+//	SListPrint(pheader);
+//	
+//	return 0;
+//}
 
-SLNode* BuyListNode(SLDataType d)//创建一个结点 
-{
-	SLNode* newNode = (SLNode*)malloc(sizeof(SLNode));
-	if (newNode == NULL)
-		return NULL;
-	newNode->data = d;
-	newNode->next = NULL;
-	return newNode;
-}
-
-SLNode* SListFind(SLNode* pheader,SLDataType d)
-{
-	SLNode* cur = pheader;
-	for(;cur != NULL;cur = cur->next)
-	{
-		if(cur->data == d)
-		{
-			return cur;
-		}
-	}
-	return NULL;
-} 
-
-void SListPushFront(SLNode** ppheader, SLDataType d)//头插 
-{
-	assert(ppheader);
-	SLNode* newNode = BuyListNode(d);
-	newNode->next = *ppheader;
-	*ppheader = newNode;
-}
-
-void SListPushBack(SLNode** ppheader, SLDataType d)//尾插 
-{
-	assert(ppheader);
-	SLNode* newNode = BuyListNode(d);
-
-	if (*ppheader == NULL)
-	{
-		*ppheader = newNode;
-	}
-	else
-	{
-		SLNode* tail = *ppheader;
-		while (tail->next != NULL)
-		{
-			tail = tail->next;
-		}
-		tail->next = newNode;
-	}
-}
-
-void SlistPopFront(SLNode** ppheader)//头删 
-{
-	if(*ppheader == NULL)
-		return;
-	SLNode* temp = *ppheader;
-	*ppheader = (*ppheader)->next;
-	free(temp);
-	temp = NULL;
-}
-
-void SListPopBack(SLNode** ppheader)//尾删 
-{
-	if(*ppheader == NULL)
-	{
-		return;
-	}
-	else if((*ppheader)->next == NULL)
-	{
-		free(*ppheader);
-		*ppheader = NULL;
-		return;
-	} 
-		
-	SLNode* prev = NULL;
-	SLNode* tail = *ppheader;
-	for(;tail->next != NULL;tail = tail->next)
-	{
-		prev = tail;
-	}
-	free(tail);
-	prev->next = NULL;
-} 
-
-void SListInsert(SLNode** ppheader,SLNode* pos,SLDataType d)//在pos的前面插入 
-{
-	assert(ppheader && pos);
-	if(pos == *ppheader)
-	{
-		SListPushFront(ppheader,d);
-	}
-	else
-	{
-		SLNode* newNode = BuyListNode(d);
-		SLNode* prev = *ppheader;
-		for(;prev->next != pos;prev = prev->next);
-		prev->next = newNode;
-		newNode->next = pos;
-	}
-} 
-
-void SListErase(SLNode** ppheader,SLNode* pos)//删除pos位置的值 
-{
-	assert(ppheader && pos);
-	if(pos == *ppheader)
-	{
-		SlistPopFront(ppheader);
-	}
-	else
-	{
-		SLNode* prev = *ppheader;
-		for(;prev->next != pos;prev = prev->next);
-		prev->next = pos->next;
-		free(pos);
-		pos = NULL;
-	}
-} 
-
-void SListPrint(SLNode* pheader)
-{
-	for (SLNode* cur = pheader; cur != NULL; cur = cur->next)
-	{
-		printf("%d ", cur->data);
-	}
-	printf("\n");
-}
-
-int main()
-{
-	SLNode* pheader = NULL;
-	SListPushBack(&pheader, 1);
-	SListPushBack(&pheader, 2);
-	SListPushBack(&pheader, 3);
-	SListPushBack(&pheader, 4);
-	SListPushFront(&pheader,0);
-
-	SListPrint(pheader);
-	
-	//在3前面插入30
-	SLNode* pos = SListFind(pheader,3);
-	if(pos)
-		SListInsert(&pheader,pos,30); 
-	
-	
-	pos = SListFind(pheader,0);
-	if(pos)
-		SListErase(&pheader,pos);
-	
-	SListPrint(pheader);
-	
-	return 0;
-}
-*/
 
 
 /*
 单链表的缺陷:
 1. 与数组相比，在链表中存储元素需要更多的内存。 因为在链表中，每个节点都包含一个指针，并且它本身需要额外的内存。
-2.在链表中很难遍历元素或节点。 我们不能像按索引在数组中那样随机访问任何元素。 
+2. 在链表中很难遍历元素或节点。 我们不能像按索引在数组中那样随机访问任何元素。 
 例如，如果我们要访问位置n处的节点，则必须遍历它之前的所有节点。
 3.单链表无法反向遍历 
 */
@@ -210,177 +206,216 @@ int main()
 
 
 //带头双向循环链表C语言实现
+//#define MULTIPLEXING//复用
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<assert.h>
+//typedef int LDataType;
+//typedef struct ListNode
+//{
+//	struct ListNode* prev;
+//	struct ListNode* next; 
+//	LDataType data; 
+//}ListNode;
+//ListNode* BuyListNode(LDataType d)//创建一个结点 
+//{
+//	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+//	if (newNode == NULL)
+//	{
+//		perror("malloc fail:");
+//		return NULL;
+//	}
+//	newNode->data = d;
+//	newNode->prev = NULL;
+//	newNode->next = NULL;
+//	return newNode;
+//}
+//ListNode* ListInit()//不可使用头结点的数据域存储链表的长度(链表的数据域不一定是int类型)
+//{
+//	ListNode* pheader = BuyListNode(-1);
+//	pheader->prev = pheader;
+//	pheader->next = pheader;
+//	return pheader;
+//}
+//void ListDestory(ListNode* pheader)
+//{
+//	assert(pheader);
+//	for(ListNode* cur = pheader->next;cur != pheader;)
+//	{
+//		ListNode* next = cur->next;
+//		free(cur);
+//		cur = next;
+//	}
+//	free(pheader);
+//	pheader = NULL;
+//}
+//ListNode* ListFind(ListNode* pheader,LDataType d)
+//{
+//	assert(pheader);
+//	for(ListNode* cur = pheader->next;cur != pheader;cur = cur->next)
+//	{
+//		if(cur->data == d)
+//		{
+//			return cur;
+//		}
+//	}
+//	return NULL;
+//} 
+//void ListInsert(ListNode* pos,LDataType d)//在pos位置之前插入 
+//{
+//	assert(pos);
+//	ListNode* newNode = BuyListNode(d);
+//	newNode->next = pos;
+//	newNode->prev = pos->prev;
+//	(pos->prev)->next = newNode;
+//	pos->prev = newNode;
+//}
+//void ListErase(ListNode* pos)//删除pos位置的结点 
+//{
+//	assert(pos);
+//	(pos->prev)->next = pos->next;
+//	(pos->next)->prev = pos->prev;
+//	free(pos);
+//	pos = NULL;
+//}
+//void ListPushFront(ListNode* pheader,LDataType d)
+//{
+//#ifdef MULTIPLEXING
+//	assert(pheader);
+//	ListInsert(pheader->next, d);
+//#else
+//	assert(pheader);
+//	ListNode* newNode = BuyListNode(d);
+//	newNode->next = pheader->next;
+//	(pheader->next)->prev = newNode;
+//	pheader->next = newNode;
+//	newNode->prev = pheader;
+//#endif
+//	
+//}
+//void ListPushBack(ListNode* pheader,LDataType d)
+//{
+//#ifdef MULTIPLEXING
+//	assert(pheader);
+//	ListInsert(pheader, d);
+//#else
+//	assert(pheader);
+//	ListNode* newNode = BuyListNode(d);
+//	(pheader->prev)->next = newNode;
+//	newNode->prev = pheader->prev;
+//	newNode->next = pheader;
+//	pheader->prev = newNode;
+//#endif
+//}
+//void ListPopFront(ListNode* pheader)
+//{
+//#ifdef MULTIPLEXING
+//	assert(pheader);
+//	assert(pheader->next != pheader);//链表不可为空
+//	ListErase(pheader->next);
+//#else
+//	assert(pheader);
+//	assert(pheader->next != pheader);
+//	ListNode* first = pheader->next;
+//	(first->next)->prev = pheader; 
+//	pheader->next = first->next;
+//	free(first);
+//	first = NULL;
+//#endif 	
+//}
+//void ListPopBack(ListNode* pheader)
+//{
+//#ifdef MULTIPLEXING
+//	assert(pheader);
+//	assert(pheader->next != pheader);
+//	ListErase(pheader->prev);
+//#else
+//	assert(pheader);
+//	assert(pheader->next != pheader);//链表不可为空
+//	ListNode* tail = pheader->prev;
+//	(tail->prev)->next =pheader;
+//	pheader->prev = tail->prev;
+//	free(tail);
+//	tail = NULL;
+//#endif
+//}
+//void for_each(ListNode* pheader)
+//{
+//	assert(pheader);
+//	for (ListNode* cur = pheader->next; cur != pheader; cur = cur->next)
+//	{
+//		printf("%d ", cur->data);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	ListNode* pheader = ListInit();
+//
+//	ListPushBack(pheader,3);
+//	ListPushBack(pheader,2);
+//	ListPushBack(pheader,1);
+//	ListPushBack(pheader,0);
+//	ListPushFront(pheader,4);
+//	ListPushFront(pheader,5);
+//	for_each(pheader);
+//	
+//	ListPopFront(pheader);
+//	ListPopBack(pheader);
+//	for_each(pheader);
+//	
+//	ListNode* pos = ListFind(pheader,2);
+//	if(pos)
+//	{
+//		ListInsert(pos,200); 
+//	}
+//	for_each(pheader);
+//	
+//	pos = ListFind(pheader,200);
+//	if(pos)
+//	{
+//		ListErase(pos);
+//	}
+//	for_each(pheader);
+//	
+//	ListDestory(pheader);
+//	pheader = NULL;
+//	return 0;
+//} 
+
+
+
+
+
+
+
+
+
+
+
+//顺序表与链表的区别
 /*
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-typedef int LDataType;
-typedef struct ListNode
-{
-	struct ListNode* prev;//前驱 
-	struct ListNode* next;//后继 
-	LDataType data; 
-}ListNode;
-ListNode* BuyListNode(LDataType d)//创建一个结点 
-{
-	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
-	if (newNode == NULL)
-		return NULL;
-	newNode->data = d;
-	newNode->prev = NULL;
-	newNode->next = NULL;
-	return newNode;
-}
-void for_each(ListNode* pheader)
-{
-	assert(pheader);
-	for(ListNode* cur = pheader->next;cur != pheader;cur = cur->next)
-	{
-		printf("%d ",cur->data);
-	}
-	printf("\n");
-}
+存储空间:
+顺序表存储空间连续								支持随机访问O(1)
+链表的逻辑结构上连续，但物理结构上不一定连续	不支持随机访问O(n) (使用二分查找等算法时，时间复杂度过大)
 
-ListNode* ListInit()
-{
-	ListNode* pheader = BuyListNode(0);
-	pheader->prev = pheader;
-	pheader->next = pheader;
-	return pheader;
-} 
-void ListDestory(ListNode* pheader)
-{
-	assert(pheader);
-	for(ListNode* cur = pheader->next;cur != pheader;)
-	{
-		ListNode* next = cur->next;
-		free(cur);
-		cur = next;
-	}
-	free(pheader);
-	pheader = NULL;
-}
+插入与删除操作:
+顺序表可能需要移动元素，时间复杂度为O(n)，	插入时可能需要扩容，可能存在空间浪费(有一定性能消耗)
+链表只需改动指针指向即可					无容量概念，不会浪费空间
 
-ListNode* ListFind(ListNode* pheader,LDataType d)
-{
-	assert(pheader);
-	for(ListNode* cur = pheader->next;cur != pheader;cur = cur->next)
-	{
-		if(cur->data == d)
-		{
-			return cur;
-		}
-	}
-	return NULL;
-} 
-void ListInsert(ListNode* pos,LDataType d)//在pos位置之前插入 
-{
-	assert(pos);
-	ListNode* newNode = BuyListNode(d);
-	newNode->next = pos;
-	newNode->prev = pos->prev;
-	(pos->prev)->next = newNode;
-	pos->prev = newNode;
-}
-void ListErase(ListNode* pos)//删除pos位置的值 
-{
-	assert(pos);
-	(pos->prev)->next = pos->next;
-	(pos->next)->prev = pos->prev;
-	free(pos);
-	pos = NULL;
-}
+应用场景:
+顺序表适用于 元素高效存储、频繁访问
+链表适用于	 任意位置插入删除和删除频繁
 
-void ListPushFront(ListNode* pheader,LDataType d)
-{
-	//assert(pheader);
-	//ListNode* newNode = BuyListNode(d);
-	//newNode->next = pheader->next;
-	//(pheader->next)->prev = newNode;
-	//pheader->next = newNode;
-	//newNode->prev = pheader;
-
-	assert(pheader);
-	ListInsert(pheader->next,d);
-}
-void ListPushBack(ListNode* pheader,LDataType d)
-{
-	//assert(pheader);
-	//ListNode* newNode = BuyListNode(d);
-	//(pheader->prev)->next = newNode;
-	//newNode->prev = pheader->prev;
-	//newNode->next = pheader;
-	//pheader->prev = newNode;
-	
-	ListInsert(pheader,d);
-}
-void ListPopFront(ListNode* pheader)
-{
-	//assert(pheader);
-	//assert(pheader->next != pheader);
-	//ListNode* first = pheader->next;
-	//(first->next)->prev = pheader; 
-	//pheader->next = first->next;
-	//free(first);
-	//first = NULL;
-	
-	assert(pheader);
-	ListErase(pheader->next);
-}
-void ListPopBack(ListNode* pheader)
-{
-	//assert(pheader);
-	//assert(pheader->next != pheader);
-	//ListNode* tail = pheader->prev;
-	//(tail->prev)->next =pheader;
-	//pheader->prev = tail->prev;
-	//free(tail);
-	//tail = NULL;
-	
-	assert(pheader);
-	ListErase(pheader->prev); 
-}
-
-
-int main()
-{
-	ListNode* pheader = ListInit();
-
-	ListPushBack(pheader,3);
-	ListPushBack(pheader,2);
-	ListPushBack(pheader,1);
-	ListPushBack(pheader,0);
-	ListPushFront(pheader,4);
-	ListPushFront(pheader,5);
-	for_each(pheader);
-	
-	ListPopFront(pheader);
-	ListPopBack(pheader);
-	for_each(pheader);
-	
-	ListNode* pos = ListFind(pheader,2);
-	if(pos)
-	{
-		ListInsert(pos,200); 
-	}
-	for_each(pheader);
-	
-	pos = ListFind(pheader,200);
-	if(pos)
-	{
-		ListErase(pos);
-	}
-	for_each(pheader);
-	
-	ListDestory(pheader);
-	return 0;
-} 
+缓存利用率:
+顺序表CPU高速缓存命中率高
+链表CPU高速缓存命中率低
 */
 
-/*
-带头双向循环链表:
-最优的链表结构，在任意位置的插入删除(不包括插入删除结点的查找)都为O(1) 
-*/
+
+
+
 
 
 
