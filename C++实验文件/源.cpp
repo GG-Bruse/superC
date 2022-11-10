c031ba37e467dc11f5d6dc4e46e39c01c6ab5643
GG-Bruse <BJY811@outlook.com>
2022-11-11 01:29:11 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -12544,3 +12544,95 @@ int getMin() 获取堆栈中的最小元素
 
 
 
+
+
+//bool PrevOrderCompare(struct TreeNode* root, int BenchMark)
+//{
+//    if (root == NULL) return true;
+//    return (root->val == BenchMark)
+//        && PrevOrderCompare(root->left, BenchMark)
+//        && PrevOrderCompare(root->right, BenchMark);
+//}
+//bool isUnivalTree(struct TreeNode* root) {
+//    if (root == NULL) return true;
+//    return PrevOrderCompare(root, root->val);
+//}
+
+
+
+
+
+
+
+//bool isUnivalTree(struct TreeNode* root) {
+//    if (root == NULL) return true;
+//    if (root->left != NULL && root->left->val != root->val) return false;
+//    if (root->right != NULL && root->right->val != root->val) return false;
+//    return isUnivalTree(root->left) && isUnivalTree(root->right);
+//}
+
+
+
+
+
+
+
+
+/*
+给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
+*/
+//bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL) return true;
+//    if (p == NULL && q != NULL) return false;
+//    if (p != NULL && q == NULL) return false;
+//    if (p->val != q->val) return false;
+//    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+//}
+
+
+
+
+
+
+
+
+
+
+/*
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+*/
+//bool Symmetric(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL) return true;
+//    if (p == NULL && q != NULL) return false;
+//    if (p != NULL && q == NULL) return false;
+//    if (p->val != q->val) return false;
+//    return Symmetric(p->left, q->right) && Symmetric(p->right, q->left);
+//}
+//bool isSymmetric(struct TreeNode* root) {
+//    return Symmetric(root->left, root->right);
+//}
+
+
+
+
+
+
+/*
+给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
+
+二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树
+*/
+//bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//    if (p == NULL && q == NULL) return true;
+//    if (p == NULL && q != NULL) return false;
+//    if (p != NULL && q == NULL) return false;
+//    if (p->val != q->val) return false;
+//    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+//}
+//bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+//    if (root == NULL) return false;
+//    if (isSameTree(root, subRoot)) return true;
+//    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+//}
