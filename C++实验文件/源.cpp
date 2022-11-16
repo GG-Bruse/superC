48b1ea4abfd7d27dc376a59c7c62a7bae9f461b9
GG-Bruse <BJY811@outlook.com>
2022-11-16 23:41:09 +0800

    C++_exercise

--- C++实验文件/源.cpp
+++ C++实验文件/源.cpp
@@ -13319,3 +13319,100 @@ int getMin() 获取堆栈中的最小元素
 //        return n1 | n2;
 //    }
 //};
+
+
+
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//struct list_node {
+//    int val;
+//    struct list_node* next;
+//};
+//
+//list_node* input_list(void)
+//{
+//    int n, val;
+//    list_node* phead = new list_node();
+//    list_node* cur_pnode = phead;
+//    scanf("%d", &n);
+//    for (int i = 1; i <= n; ++i) {
+//        scanf("%d", &val);
+//        if (i == 1) {
+//            cur_pnode->val = val;
+//            cur_pnode->next = NULL;
+//        }
+//        else {
+//            list_node* new_pnode = new list_node();
+//            new_pnode->val = val;
+//            new_pnode->next = NULL;
+//            cur_pnode->next = new_pnode;
+//            cur_pnode = new_pnode;
+//        }
+//    }
+//    return phead;
+//}
+//
+//
+//list_node* reverse_list(list_node* head, int L, int R)
+//{
+//    list_node* dummyNode = (list_node*)malloc(sizeof(list_node));
+//    dummyNode->next = head;
+//    list_node* pre = dummyNode;
+//    for (int i = 0; i < L - 1; i++) {
+//        pre = pre->next;
+//    }
+//    list_node* cur = pre->next;
+//    list_node* next;
+//    for (int i = 0; i < R - L; i++) {
+//        next = cur->next;
+//        cur->next = next->next;
+//        next->next = pre->next;
+//        pre->next = next;
+//    }
+//    return dummyNode->next;
+//}
+//
+//void print_list(list_node* head)
+//{
+//    while (head != NULL) {
+//        printf("%d ", head->val);
+//        head = head->next;
+//    }
+//    puts("");
+//}
+//
+//
+//int main()
+//{
+//    int L, R;
+//    list_node* head = input_list();
+//    scanf("%d%d", &L, &R);
+//    list_node* new_head = reverse_list(head, L, R);
+//    print_list(new_head);
+//    return 0;
+//}
+
+
+
+
+
+
+//#include <iostream>
+//#include <algorithm>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//    int n = 0;
+//    while (cin >> n) {
+//        if (n == 0) break;
+//        size_t total = 0, sum = 0;
+//        sum = pow(5, n) - 4;
+//        total = pow(4, n) + n - 4;
+//        cout << sum << " " << total << endl;
+//    }
+//    return 0;
+//}
