114c52d90ade7f45de3cc60dfdd1d7b1b377f65f
包佳毅 <BJY811@outlook.com>
2023-08-19 11:03:07 +0800

    dp_OJ_Exercise

--- OJ练习/动态规划专题.cpp
+++ OJ练习/动态规划专题.cpp
@@ -84,6 +84,43 @@
 
 
 
+/*************************************************************** 不同路径（LeetCode）*******************************************************/
+/*
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+问总共有多少条不同的路径？
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+// class Solution_1 {
+// public:
+//     int uniquePaths(int m, int n) 
+//     {
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+//         dp[0][0] = 1;
+//         for(int i = 0; i < m; ++i) {
+//             for(int j = 0; j < n; ++j) {
+//                 if(i - 1 >= 0) dp[i][j] += dp[i - 1][j];
+//                 if(j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+//             }
+//         }
+//         return dp[m - 1][n - 1];
+//     }
+// };
+// class Solution_2 {
+// public:
+//     int uniquePaths(int m, int n)
+//     {
+//         //多加一行一列, 优化边界问题
+//         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+//         dp[0][1] = 1;
+//         for (int i = 1; i <= m; ++i)
+//             for (int j = 1; j <= n; ++j)
+//                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//         return dp[m][n];
+//     }
+// };
 
 
 
@@ -96,3 +133,106 @@
 
 
 
+
+
+/************************************************************* 不同路径II（LeetCode）**********************************************************/
+/*
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+网格中的障碍物和空位置分别用 1 和 0 来表示
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution_1 {
+//public:
+//    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+//    {
+//        vector<vector<int>> dp(obstacleGrid.size(), vector<int>(obstacleGrid[0].size(), 0));
+//        dp[0][0] = 1;
+//        for (int i = 0; i < obstacleGrid.size(); ++i)
+//        {
+//            for (int j = 0; j < obstacleGrid[i].size(); ++j)
+//            {
+//                if (i - 1 >= 0 && obstacleGrid[i - 1][j] != 1) dp[i][j] += dp[i - 1][j];
+//                if (j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) dp[i][j] += dp[i][j - 1];
+//                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+//            }
+//        }
+//        return dp.back().back();
+//    }
+//};
+//class Solution_2 {
+//public:
+//    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid)
+//    {
+//        //多出一行一列用于优化边界问题
+//        int row = obstacleGrid.size(), col = obstacleGrid[0].size();
+//        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+//        dp[0][1] = 1;//需确保后续填表正确
+//        for (int i = 1; i <= row; ++i)
+//            for (int j = 1; j <= col; ++j)
+//                if (obstacleGrid[i - 1][j - 1] != 1) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//        return dp[row][col];
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*************************************************************** 礼物的最大价值（LeetCode）***************************************************/
+/*
+在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
+你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
+给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution_1 {
+//public:
+//    int maxValue(vector<vector<int>>& grid)
+//    {
+//        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), 0));
+//        dp[0][0] = grid[0][0];
+//        for (int i = 0; i < dp.size(); ++i)
+//        {
+//            for (int j = 0; j < dp[i].size(); ++j)
+//            {
+//                int up = 0, left = 0;
+//                if (i - 1 >= 0) up = dp[i - 1][j];
+//                if (j - 1 >= 0) left = dp[i][j - 1];
+//                dp[i][j] = max(up, left) + grid[i][j];
+//            }
+//        }
+//        return dp.back().back();
+//    }
+//};
+//class Solution_2 {
+//public:
+//    int maxValue(vector<vector<int>>& grid)
+//    {
+//        //多出一行一列用于优化边界问题
+//        int row = grid.size(), col = grid[0].size();
+//        vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+//        for (int i = 1; i <= row; ++i)
+//            for (int j = 1; j <= col; ++j)
+//                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+//        return dp[row][col];
+//    }
+//};
