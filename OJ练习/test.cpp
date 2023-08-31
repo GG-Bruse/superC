9640da3ff8e169cbd079bced52b9e61d9b058ff6
包佳毅 <BJY811@outlook.com>
2023-08-31 12:07:09 +0800

    OJ_exercise

--- OJ练习/test.cpp
+++ OJ练习/test.cpp
@@ -1913,3 +1913,48 @@ D(1) = 0, D(2) = 1
 //    }
 //    return 0;
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************************** 最后一块石头的重量（LeetCode）*************************************************/
+/*
+有一堆石头，每块石头的重量都是正整数
+每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+如果 x == y，那么两块石头都会被完全粉碎
+如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x
+最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0
+*/
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//using namespace std;
+//class Solution {
+//public:
+//    int lastStoneWeight(vector<int>& stones)
+//    {
+//        priority_queue<int> qe;
+//        for (auto& it : stones) qe.push(it);
+//
+//        while (qe.size() > 1)
+//        {
+//            int x = qe.top();
+//            qe.pop();
+//            int y = qe.top();
+//            qe.pop();
+//            if (x > y) qe.push(x - y);//因为使用的是大堆,不可能出现x < y的情况;若x == y,不处理则相当于丢弃
+//        }
+//        return qe.empty() ? 0 : qe.top();
+//    }
+//};
+
