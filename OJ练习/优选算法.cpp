2dbb1c3bedb78cc58a39d1c3dd1c4c825abd08f8
包佳毅 <BJY811@outlook.com>
2023-09-23 13:12:54 +0800

    OJ_TEST

--- OJ练习/优选算法.cpp
+++ OJ练习/优选算法.cpp
@@ -23,6 +23,8 @@
 
 
 
+
+
 /************************************************** 复写零（LeetCode）***********************************************/
 /*
 给你一个长度固定的整数数组 arr ，请你将该数组中出现的每个零都复写一遍，并将其余的元素向右平移
@@ -57,6 +59,10 @@
 //        }
 //    }
 //};
+
+
+
+
 
 
 
@@ -120,5 +126,240 @@
 //            Set.insert(ret);
 //            n = ret;
 //        }
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+/************************************************** 盛最多水的容器（LeetCode）******************************************/
+/*
+给定一个长度为n的整数数组height 。有n条垂线，第i条线的两个端点是 (i, 0) 和 (i, height[i]) 
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水
+返回容器可以储存的最大水量
+说明：你不能倾斜容器
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution {
+//public:
+//    /*
+//    V = height * width
+//    left和right向中间移动, 导致width减小, 若想令V增大, 必须让高度增加, 于是每次将高度较低的丢弃
+//    */
+//    int maxArea(vector<int>& height)
+//    {
+//        int ret = 0;
+//        int left = 0, right = height.size() - 1;
+//        while (left < right) {
+//            ret = max(ret, (right - left) * min(height[left], height[right]));
+//            if (height[left] >= height[right]) --right;
+//            else /*if(height[left] < height[right])*/ ++left;
+//        }
+//        return ret;
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+
+/*********************************************** 有效三角形的个数（LeetCode）*******************************************/
+/*
+给定一个包含非负整数的数组nums ，返回其中可以组成三角形三条边的三元组个数
+*/
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//class Solution {
+//public:
+//    int triangleNumber(vector<int>& nums)
+//    {
+//        int size = nums.size();
+//        sort(nums.begin(), nums.end());
+//
+//        int ret = 0;
+//        for (int i = size - 1; i >= 2; --i)//先固定一个最大数
+//        {
+//            int left = 0, right = i - 1;
+//            while (left < right)
+//            {
+//                if (nums[left] + nums[right] > nums[i]) {
+//                    ret += right - left;
+//                    --right;
+//                }
+//                else /*if(nums[left] + nums[right] <= nums[i])*/ {
+//                    ++left;
+//                }
+//            }
+//        }
+//        return ret;
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************************** 查找总价格为目标值的两个商品（LeetCode）**********************************/
+/*
+购物车内的商品价格按照升序记录于数组price。请在购物车中找到两个商品的价格总和刚好是target
+若存在多种情况，返回任一结果即可
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution {
+//public:
+//    vector<int> twoSum(vector<int>& price, int target)
+//    {
+//        int size = price.size();
+//        int left = 0, right = size - 1;
+//        while (left < right)
+//        {
+//            int sum = price[left] + price[right];
+//            if (sum > target) --right;
+//            else if (sum < target) ++left;
+//            else /* if(sum == target) */ return { price[left], price[right] };
+//        }
+//        return { 0,0 };
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+/*************************************************** 三数之和（LeetCode）***********************************************/
+/*
+给你一个整数数组nums，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+同时还满足 nums[i] + nums[j] + nums[k] == 0
+请你返回所有和为 0 且不重复的三元组
+注意：答案中不可以包含重复的三元组
+*/
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//class Solution {
+//public:
+//    vector<vector<int>> threeSum(vector<int>& nums)
+//    {
+//        int size = nums.size();
+//        sort(nums.begin(), nums.end());
+//
+//        vector<vector<int>> ret;
+//        for (int i = size - 1; i >= 2;)//固定最后一个数
+//        {
+//            if (nums[i] < 0) return ret;//最后一个数都为负数, 此时已经不可能找到为0的组合了
+//            int left = 0, right = i - 1;
+//            int target = -nums[i];
+//            while (left < right)
+//            {
+//                int sum = nums[left] + nums[right];
+//                if (sum > target) --right;
+//                else if (sum < target) ++left;
+//                else /*sum == 0*/ {
+//                    ret.push_back({ nums[left], nums[right], nums[i] });
+//                    ++left, --right;
+//                    //去重
+//                    while (left < right && nums[left] == nums[left - 1]) left++;
+//                    while (left < right && nums[right] == nums[right + 1]) right--;
+//                }
+//            }
+//            --i;
+//            while (i >= 2 && nums[i] == nums[i + 1]) i--;//去重
+//        }
+//        return ret;
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+/************************************************* 四数之和（LeetCode）*************************************************/
+/*
+给你一个由n个整数组成的数组nums，和一个目标值target
+请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] 
+（若两个四元组元素一一对应，则认为两个四元组重复）：
+0 <= a, b, c, d < n
+a、b、c 和 d 互不相同
+nums[a] + nums[b] + nums[c] + nums[d] == target
+你可以按任意顺序返回答案
+*/
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//class Solution {
+//public:
+//    vector<vector<int>> fourSum(vector<int>& nums, int target)
+//    {
+//        int size = nums.size();
+//        sort(nums.begin(), nums.end());
+//        vector<vector<int>> ret;
+//
+//        for (int i = size - 1; i >= 3;)//先固定最后一个数
+//        {
+//            for (int j = i - 1; j >= 2;)//固定倒数第二个数
+//            {
+//                //双指针
+//                int left = 0, right = j - 1;
+//                long long aim = (long long)target - nums[i] - nums[j];
+//                while (left < right)
+//                {
+//                    int sum = nums[left] + nums[right];
+//                    if (sum > aim) --right;
+//                    else if (sum < aim) ++left;
+//                    else /*sum == 0*/ {
+//                        ret.push_back({ nums[left++], nums[right--], nums[i], nums[j] });
+//                        //去重
+//                        while (left < right && nums[left] == nums[left - 1]) left++;
+//                        while (left < right && nums[right] == nums[right + 1]) right--;
+//                    }
+//                }
+//                --j;
+//                while (j >= 2 && nums[j] == nums[j + 1]) --j;
+//            }
+//            --i;
+//            while (i >= 3 && nums[i] == nums[i + 1]) --i;
+//        }
+//        return ret;
 //    }
 //};
