015ab4306786393a196e0f5a1a483c96c8340103
包佳毅 <BJY811@outlook.com>
2023-10-21 13:16:06 +0800

    OJ_TEST

--- OJ练习/贪心算法专题.cpp
+++ OJ练习/贪心算法专题.cpp
@@ -76,3 +76,75 @@
 //        return count;
 //    }
 //};
+
+
+
+
+
+
+
+
+
+/*********************************************** 最大数（LeetCode）***********************************************/
+/*
+给定一组非负整数nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数
+注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数
+*/
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//class Solution {
+//public:
+//    string largestNumber(vector<int>& nums)
+//    {
+//        sort(nums.begin(), nums.end(), [](int num1, int num2) {
+//            return to_string(num1) + to_string(num2) > to_string(num2) + to_string(num1);
+//        });
+//        string ret = "";
+//        for (int& it : nums) ret += to_string(it);
+//        if (ret[0] == '0') return "0";
+//        return ret;
+//    }
+//};
+
+
+
+
+
+
+
+
+
+/******************************************** 摆动序列（LeetCode）************************************************/
+/*
+如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。第一个差（如果存在的话）可能是正数或负数
+仅有一个元素或者含两个不等元素的序列也视作摆动序列
+例如， [1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3) 是正负交替出现的
+相反，[1, 4, 7, 2, 5] 和 [1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，
+第二个序列是因为它的最后一个差值为零。
+子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序
+给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution {
+//public:
+//    int wiggleMaxLength(vector<int>& nums)
+//    {
+//        int size = nums.size();
+//        if (size < 2) return size;
+//
+//        int maxLength = 0, left = 0;
+//        for (int i = 0; i < size - 1; ++i)
+//        {
+//            int right = nums[i + 1] - nums[i];//接下来的趋势
+//            if (right == 0) continue;
+//            if (left * right <= 0) ++maxLength;//左右趋势相反,即存在波峰或波谷
+//            left = right;
+//        }
+//        return maxLength + 1;
+//    }
+//};
