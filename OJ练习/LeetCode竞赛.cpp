479f62e2c471d8d33e4562ce62d029a0c6db3d13
包佳毅 <BJY811@outlook.com>
2023-10-24 16:33:46 +0800

    LeetCode_OJ

--- OJ练习/LeetCode竞赛.cpp
+++ OJ练习/LeetCode竞赛.cpp
@@ -30,3 +30,52 @@ nums[i] < nums[j] 且 nums[k] < nums[j]
 //        return minTop == INT_MAX ? -1 : minTop;
 //    }
 //};
+
+
+
+
+
+
+
+
+
+
+/****************************************** 合法分组的最少组数（LeetCode）*************************************/
+/*
+给你一个长度为n下标从0开始的整数数组nums
+我们想将下标进行分组，使得 [0, n - 1] 内所有下标i都恰好被分到其中一组
+如果以下条件成立，我们说这个分组方案是合法的：
+对于每个组 g ，同一组内所有下标在nums中对应的数值都相等
+对于任意两个组 g1 和 g2 ，两个组中下标数量的差值不超过1
+请你返回一个整数，表示得到一个合法分组方案的 最少 组数
+*/
+//#include <iostream>
+//#include <vector>
+//#include <unordered_map>
+//using namespace std;
+//class Solution {
+//public:
+//    int minGroupsForValidAssignment(vector<int>& nums)
+//    {
+//        unordered_map<int, int> counts;//记录每个数出现的频次
+//        for (auto& it : nums) ++counts[it];
+//        //求出最小组中元素的个数
+//        int minSize = INT_MAX;
+//        for (auto& [_, cnt] : counts) minSize = min(minSize, cnt);
+//        //枚举每一种分组情况
+//        for (int k = minSize; k > 0; --k)//最多分为minSize组, 每组中一个元素
+//        {
+//            int ret = 0;
+//            for (auto& [_, cnt] : counts)
+//            {
+//                if (cnt / k < cnt % k) {//无法分成k和k+1组
+//                    ret = 0;
+//                    break;
+//                }
+//                ret += (cnt + k) / (k + 1);
+//            }
+//            if (ret) return ret;
+//        }
+//        return -1;
+//    }
+//};
