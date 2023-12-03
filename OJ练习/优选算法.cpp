5b6e7a569d0ce1abb37d5e2d7a40d877376ac6a4
包佳毅 <BJY811@outlook.com>
2023-12-03 09:11:08 +0800

    TEST_OJ

--- OJ练习/优选算法.cpp
+++ OJ练习/优选算法.cpp
@@ -4429,7 +4429,7 @@ sk == endWord
 
 
 
-/*********************************************** 旋转图像（LeetCode）*************************************************/
+/*********************************************** 旋转图像（LeetCode）***********************************************/
 /*
 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度
 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像
@@ -4449,5 +4449,68 @@ sk == endWord
 //        for (int i = 0; i < size; ++i)
 //            for (int j = 0; j < i; ++j)
 //                swap(matrix[i][j], matrix[j][i]);
+//    }
+//};
+
+
+
+
+
+
+
+
+
+
+/********************************************** 腐烂的橘子（LeetCode）**********************************************/
+/*
+在给定的 m x n 网格 grid 中，每个单元格可以有以下三个值之一：
+值 0 代表空单元格
+值 1 代表新鲜橘子
+值 2 代表腐烂的橘子
+每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂
+返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1
+*/
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//using namespace std;
+//class Solution {
+//public:
+//    int dx[4] = { 0, 0, 1, -1 };
+//    int dy[4] = { -1, 1, 0, 0 };
+//    int orangesRotting(vector<vector<int>>& grid)
+//    {
+//        int minMinute = 0;
+//        int row = grid.size(), col = grid[0].size();
+//
+//        queue<pair<int, int>> qe;
+//        for (int i = 0; i < row; ++i)
+//            for (int j = 0; j < col; ++j)
+//                if (grid[i][j] == 2) qe.push({ i, j });
+//
+//        while (!qe.empty())
+//        {
+//            ++minMinute;
+//            int size = qe.size();
+//            while (size--)
+//            {
+//                auto [a, b] = qe.front();
+//                qe.pop();
+//                for (int k = 0; k < 4; ++k)
+//                {
+//                    int x = a + dx[k], y = b + dy[k];
+//                    if (x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 1) {
+//                        grid[x][y] = 2;
+//                        qe.push({ x, y });
+//                    }
+//                }
+//            }
+//        }
+//        int flag = false;
+//        for (int i = 0; i < row; ++i)
+//            for (int j = 0; j < col; ++j)
+//                if (grid[i][j] == 1) flag = true;
+//        if (flag) return -1;
+//        else return minMinute == 0 ? 0 : minMinute - 1;
 //    }
 //};
