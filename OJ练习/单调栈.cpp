d841c2c0071f4ac422042e635b413b1b0d9fc848
包佳毅 <BJY811@outlook.com>
2023-12-28 21:42:54 +0800

    stack_OJ

--- OJ练习/单调栈.cpp
+++ OJ练习/单调栈.cpp
@@ -108,3 +108,48 @@
 //        return ret;
 //    }
 //};
+
+
+
+
+
+
+
+
+
+
+/****************************************** 移掉K位数字（LeetCode）********************************************/
+/*
+给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小
+请你以字符串形式返回这个最小的数字
+*/
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//class Solution {
+//public:
+//    string removeKdigits(string num, int k)
+//    {
+//        vector<char> sk;
+//        for (int i = 0; i < num.size(); ++i)
+//        {
+//            while (!sk.empty() && sk.back() > num[i] && k > 0) {
+//                sk.pop_back();
+//                k -= 1;
+//            }
+//            sk.push_back(num[i]);
+//        }
+//        while (k--) sk.pop_back();
+//
+//        string ret = "";
+//        bool isLeadingZero = true;
+//        for (auto& it : sk)
+//        {
+//            if (isLeadingZero && it == '0') continue;
+//            isLeadingZero = false;
+//            ret += it;
+//        }
+//        return ret == "" ? "0" : ret;
+//    }
+//};
