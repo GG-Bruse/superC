884d14c069ad5ba39e7baad746534f3813bdd7f3
包佳毅 <BJY811@outlook.com>
2024-01-18 22:32:29 +0800

    DP_OJ

--- OJ练习/动态规划专题.cpp
+++ OJ练习/动态规划专题.cpp
@@ -3528,3 +3528,117 @@ nums[i] 的前一个元素是 nums[(i - 1 + n) % n] 。
 //        return dp[0][size + 1];
 //    }
 //};
+
+
+
+
+
+
+
+
+
+
+/******************************************** 01矩阵（LeetCode）***********************************************/
+/*
+给定一个由 0 和 1 组成的矩阵 mat ，请输出一个大小相同的矩阵
+其中每一个格子是 mat 中对应位置元素到最近的 0 的距离
+两个相邻元素间的距离为 1
+*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//class Solution {
+//public:
+//    vector<vector<int>> updateMatrix(vector<vector<int>>& mat)
+//    {
+//        int row = mat.size(), col = mat[0].size();
+//        //dp[i][j] : mat[i][j]到最近的0的距离,初始化为一个较大值
+//        vector<vector<int>> dp(row, vector<int>(col, INT_MAX / 2));
+//        //解决0位置
+//        for (int i = 0; i < row; ++i)
+//            for (int j = 0; j < col; ++j)
+//                if (mat[i][j] == 0) dp[i][j] = 0;
+//        //只能向左移动和向上移动
+//        for (int i = 0; i < row; ++i)
+//        {
+//            for (int j = 0; j < col; ++j)
+//            {
+//                if (i - 1 >= 0)
+//                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+//                if (j - 1 >= 0)
+//                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+//            }
+//        }
+//        //只能向左移动和向下移动
+//        for (int i = row - 1; i >= 0; --i)
+//        {
+//            for (int j = 0; j < col; ++j)
+//            {
+//                if (i + 1 < row)
+//                    dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+//                if (j - 1 >= 0)
+//                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+//            }
+//        }
+//        //只能向右移动和向上移动
+//        for (int i = 0; i < row; ++i)
+//        {
+//            for (int j = col - 1; j >= 0; --j)
+//            {
+//                if (i - 1 >= 0)
+//                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+//                if (j + 1 < col)
+//                    dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+//            }
+//        }
+//        //只能向右和向下移动
+//        for (int i = row - 1; i >= 0; --i)
+//        {
+//            for (int j = col - 1; j >= 0; --j)
+//            {
+//                if (i + 1 < row)
+//                    dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+//                if (j + 1 < col)
+//                    dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+//            }
+//        }
+//        return dp;
+//    }
+//};
+//优化版
+//class Solution {
+//public:
+//    vector<vector<int>> updateMatrix(vector<vector<int>>& mat)
+//    {
+//        int row = mat.size(), col = mat[0].size();
+//        //dp[i][j] : mat[i][j]到最近的0的距离,初始化为一个较大值
+//        vector<vector<int>> dp(row, vector<int>(col, INT_MAX / 2));
+//        //解决0位置
+//        for (int i = 0; i < row; ++i)
+//            for (int j = 0; j < col; ++j)
+//                if (mat[i][j] == 0) dp[i][j] = 0;
+//        //只能向左移动和向上移动
+//        for (int i = 0; i < row; ++i)
+//        {
+//            for (int j = 0; j < col; ++j)
+//            {
+//                if (i - 1 >= 0)
+//                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+//                if (j - 1 >= 0)
+//                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+//            }
+//        }
+//        //只能向右和向下移动
+//        for (int i = row - 1; i >= 0; --i)
+//        {
+//            for (int j = col - 1; j >= 0; --j)
+//            {
+//                if (i + 1 < row)
+//                    dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+//                if (j + 1 < col)
+//                    dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+//            }
+//        }
+//        return dp;
+//    }
+//};
