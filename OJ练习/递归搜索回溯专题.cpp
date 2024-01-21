b7738ec8a71000ff5242a0d91f632a545d0963f0
包佳毅 <BJY811@outlook.com>
2024-01-21 15:51:13 +0800

    TEST_OJ

--- OJ练习/递归搜索回溯专题.cpp
+++ OJ练习/递归搜索回溯专题.cpp
@@ -2185,3 +2185,81 @@ F(n) = F(n - 1) + F(n - 2)，其中 n > 1
 //        return dfs(memory, newNums, 0, size + 1);
 //    }
 //};
+
+
+
+
+
+
+
+
+
+/********************************* 填充每个节点的下一个右侧节点指针（LeetCode） *******************************/
+/*
+给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL
+初始状态下，所有 next 指针都被设置为 NULL
+*/
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//class Node {
+//public:
+//    int val;
+//    Node* left;
+//    Node* right;
+//    Node* next;
+//    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+//    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+//    Node(int _val, Node* _left, Node* _right, Node* _next)
+//        : val(_val), left(_left), right(_right), next(_next) {}
+//};
+////使用已建立的next指针更新下一层
+//class Solution1 {
+//public:
+//    Node* connect(Node* root)
+//    {
+//        if (root == nullptr) return root;
+//
+//        Node* leftMost = root;
+//        while (leftMost->left != nullptr)
+//        {
+//            Node* head = leftMost;
+//            while (head != nullptr)
+//            {
+//                head->left->next = head->right;
+//                if (head->next != nullptr)
+//                    head->right->next = head->next->left;
+//                head = head->next;
+//            }
+//            // 去下一层的最左的节点
+//            leftMost = leftMost->left;
+//        }
+//        return root;
+//    }
+//};
+////层序遍历
+//class Solution2 
+//{
+//public:
+//    Node* connect(Node* root)
+//    {
+//        if (root == nullptr) return root;
+//
+//        queue<Node*> qe;
+//        qe.push(root);
+//        while (!qe.empty())
+//        {
+//            int size = qe.size();
+//            for (int i = 0; i < size; ++i)
+//            {
+//                Node* node = qe.front();
+//                qe.pop();
+//                if (i < size - 1) node->next = qe.front();
+//                if (node->left != nullptr) qe.push(node->left);
+//                if (node->right != nullptr) qe.push(node->right);
+//            }
+//        }
+//        return root;
+//    }
+//};
